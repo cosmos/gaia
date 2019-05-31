6412582b2367fa788eaee7724bfdb80aa7537fc0
Karoly Albert Szabo <szabo.karoly.a@gmail.com>
2019-05-31 17:31:34 +0200

    add unbond command
    Signed-off-by: Karoly Albert Szabo <szabo.karoly.a@gmail.com>

--- lcd_test/testdata/setup.sh
+++ lcd_test/testdata/setup.sh
@@ -2,6 +2,7 @@
 
 PASSWORD="1234567890"
 ADDR="cosmos16xyempempp92x9hyzz9wrgf94r6j9h5f06pxxv"
+VALIDATOR="cosmosvaloper16xyempempp92x9hyzz9wrgf94r6j9h5f2w4n2l"
 CHAIN="lcd"
 GOVID="2"
 HOME="/tmp/.gaiacli"
@@ -15,3 +16,5 @@ sleep 1s
 echo ${PASSWORD} | ./build/gaiacli tx gov deposit --home ${HOME} --from ${ADDR} --chain-id ${CHAIN} ${GOVID} 1000000000stake --yes
 sleep 1s
 echo ${PASSWORD} | ./build/gaiacli tx gov vote --home ${HOME} --from ${ADDR} --yes --chain-id ${CHAIN} ${GOVID} Yes
+sleep 1s
+echo ${PASSWORD} | ./build/gaiacli tx staking unbond --home ${HOME} --from ${ADDR} ${VALIDATOR} 100stake --yes --chain-id ${CHAIN}

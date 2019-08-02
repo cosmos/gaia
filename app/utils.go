8a7c41c527db6c932a9dade48f9ab6ac6ab5bb2d
Federico Kunze <federico.kunze94@gmail.com>
2019-08-02 18:12:36 +0200

    update simulations

--- app/utils.go
+++ app/utils.go
@@ -4,12 +4,32 @@ package app
 import (
 	"io"
 
-	dbm "github.com/tendermint/tm-db"
 	"github.com/tendermint/tendermint/libs/log"
+	dbm "github.com/tendermint/tm-db"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/staking"
+)
+
+var (
+	genesisFile        string
+	paramsFile         string
+	exportParamsPath   string
+	exportParamsHeight int
+	exportStatePath    string
+	exportStatsPath    string
+	seed               int64
+	numBlocks          int
+	blockSize          int
+	enabled            bool
+	verbose            bool
+	lean               bool
+	commit             bool
+	period             int
+	onOperation        bool // TODO Remove in favor of binary search for invariant violation
+	allInvariants      bool
+	genesisTime        int64
 )
 
 // DONTCOVER

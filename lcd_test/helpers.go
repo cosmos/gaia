45c79a163868d42ef1f461edebd61c37a9091a83
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-09-13 18:44:00 -0400

    Remove NewBaseGenesisAccount from tests

--- lcd_test/helpers.go
+++ lcd_test/helpers.go
@@ -198,7 +198,7 @@ func defaultGenesis(config *tmcfg.Config, nValidators int, initAddrs []sdk.AccAd
 		totalSupply = totalSupply.Add(accTokens)
 
 		account.Coins = sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, accTokens))
-		genAccounts = append(genAccounts, app.NewBaseGenesisAccount(&account))
+		genAccounts = append(genAccounts, &account)
 	}
 
 	genesisState := app.NewDefaultGenesisState()
@@ -219,8 +219,7 @@ func defaultGenesis(config *tmcfg.Config, nValidators int, initAddrs []sdk.AccAd
 		accAuth.Coins = sdk.Coins{sdk.NewCoin(sdk.DefaultBondDenom, accTokens)}
 		totalSupply = totalSupply.Add(accTokens)
 
-		acc := app.NewBaseGenesisAccount(&accAuth)
-		genAccounts = append(genAccounts, acc)
+		genAccounts = append(genAccounts, &accAuth)
 	}
 
 	// auth genesis state: params and genesis accounts

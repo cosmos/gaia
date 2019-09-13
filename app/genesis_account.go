bb832617c6a5de103d496cfa0a0cdc3a89b236bc
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2019-09-13 11:46:22 -0400

    Update GenesisAccount

--- app/genesis_account.go
+++ app/genesis_account.go
@@ -1,9 +1,12 @@
 package app
 
 import (
+	"errors"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authexported "github.com/cosmos/cosmos-sdk/x/auth/exported"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/cosmos/cosmos-sdk/x/supply"
 )
 
 var _ authexported.GenesisAccount = (*GenesisAccount)(nil)
@@ -29,9 +32,55 @@ type GenesisAccount struct {
 	ModulePermissions []string `json:"module_permissions" yaml:"module_permissions"` // permissions of module account
 }
 
-// NewGenesisAccount creates a GenesisAccount instance from a BaseAccount.
-func NewGenesisAccount(acc *authtypes.BaseAccount) GenesisAccount {
+// NewBaseGenesisAccount creates a GenesisAccount instance from only a BaseAccount.
+func NewBaseGenesisAccount(acc *authtypes.BaseAccount) GenesisAccount {
 	return GenesisAccount{
 		BaseAccount: acc,
 	}
+}
+
+func NewGenesisAccount(
+	address sdk.AccAddress, coins, vestingAmount sdk.Coins,
+	vestingStartTime, vestingEndTime int64, module string, permissions ...string,
+) GenesisAccount {
+
+	return GenesisAccount{
+		BaseAccount: &authtypes.BaseAccount{
+			Address:       address,
+			Coins:         coins,
+			Sequence:      0,
+			AccountNumber: 0, // ignored; set by the account keeper during InitGenesis
+		},
+		OriginalVesting:   vestingAmount,
+		DelegatedFree:     sdk.Coins{},
+		DelegatedVesting:  sdk.Coins{},
+		StartTime:         vestingStartTime,
+		EndTime:           vestingEndTime,
+		ModuleName:        module,
+		ModulePermissions: permissions,
+	}
+}
+
+// Validate validates the GenesisAccount and returns an error if the account is
+// invalid.
+func (g GenesisAccount) Validate() error {
+	if !g.OriginalVesting.IsZero() {
+		if g.OriginalVesting.IsAnyGT(g.Coins) {
+			return errors.New("vesting amount cannot be greater than total amount")
+		}
+		if g.StartTime >= g.EndTime {
+			return errors.New("vesting start-time cannot be before end-time")
+		}
+	}
+
+	if g.ModuleName != "" {
+		ma := supply.ModuleAccount{
+			BaseAccount: g.BaseAccount, Name: g.ModuleName, Permissions: g.ModulePermissions,
+		}
+		if err := ma.Validate(); err != nil {
+			return err
+		}
+	}
+
+	return g.BaseAccount.Validate()
 }

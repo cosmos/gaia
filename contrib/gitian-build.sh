69d18954e0be21c9a528fca00ac2bc59b4613133
Alessio Treglia <alessio@tendermint.com>
2019-10-25 14:22:08 +0100

    Merge PR #171: Update gitian-builder repository's location

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -8,7 +8,7 @@
 set -euo pipefail
 
 GITIAN_CACHE_DIRNAME='.gitian-builder-cache'
-GO_RELEASE='1.13'
+GO_RELEASE='1.13.3'
 GO_TARBALL="go${GO_RELEASE}.linux-amd64.tar.gz"
 GO_TARBALL_URL="https://dl.google.com/go/${GO_TARBALL}"
 
@@ -16,7 +16,7 @@ GO_TARBALL_URL="https://dl.google.com/go/${GO_TARBALL}"
 
 DEFAULT_SIGN_COMMAND='gpg --detach-sign'
 DEFAULT_GAIA_SIGS=${GAIA_SIGS:-'gaia.sigs'}
-DEFAULT_GITIAN_REPO='https://github.com/cosmos/gitian-builder'
+DEFAULT_GITIAN_REPO='https://github.com/tendermint/gitian-builder'
 DEFAULT_GBUILD_FLAGS=''
 DEFAULT_SIGS_REPO='https://github.com/cosmos/gaia.sigs'
 

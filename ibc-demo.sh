a35b18bb2b96f42be3a46204adf52004edf70287
mossid <torecursedivine@gmail.com>
2019-11-01 22:35:53 -0700

    update go.mod, make shell to use --source on recv

--- ibc-demo.sh
+++ ibc-demo.sh
@@ -171,7 +171,8 @@ gaiacli \
   --timeout $TIMEOUT \
   --from n1 \
   --node2 tcp://localhost:26657 \
-  --chain-id2 ibc0
+  --chain-id2 ibc0 \
+  --source
 
 echo "Account after:"
 gaiacli --home ibc1/n0/gaiacli q account $DEST

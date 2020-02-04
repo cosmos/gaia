b6f2c31e791334c929fb513a8942bb49ce8ad162
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-02-04 13:15:18 -0500

    Fix TestGaiaCLIFeesDeduction

--- cli_test/cli_test.go
+++ cli_test/cli_test.go
@@ -188,7 +188,7 @@ func TestGaiaCLIFeesDeduction(t *testing.T) {
 	success, stdOut, _ := f.TxSend(
 		keyFoo, barAddr, sdk.NewCoin(fooDenom, largeCoins),
 		fmt.Sprintf("--fees=%s", sdk.NewInt64Coin(feeDenom, 2)), "-y")
-	require.Contains(t, stdOut, "insufficient account funds")
+	require.Contains(t, stdOut, "insufficient funds")
 	require.True(t, success)
 
 	// Wait for a block

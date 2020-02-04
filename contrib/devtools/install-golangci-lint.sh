8339f708fe4ed4a8262dc3d83fbdb4757f538689
Erik Grinaker <erik@tendermint.com>
2020-02-04 12:35:30 +0100

    Removed debug output

--- contrib/devtools/install-golangci-lint.sh
+++ contrib/devtools/install-golangci-lint.sh
@@ -20,7 +20,6 @@ echo "Downloading golangci-lint ${VERSION} installer ..." >&2
 "${CURL}" -sfL "https://raw.githubusercontent.com/golangci/golangci-lint/${VERSION}/install.sh" > "${installer}"
 
 echo "Checking hashsum ..." >&2
-echo "${installer}"
 [ "${HASHSUM}" = "$(f_sha256 ${installer})" ]
 chmod +x "${installer}"
 

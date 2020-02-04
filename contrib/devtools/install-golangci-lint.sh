16893ad488b05ae801852dade7690c505f6fb46d
Erik Grinaker <erik@tendermint.com>
2020-02-04 12:34:41 +0100

    Makefile: use specific golangci-lint version

--- contrib/devtools/install-golangci-lint.sh
+++ contrib/devtools/install-golangci-lint.sh
@@ -7,7 +7,8 @@ trap "rm -f ${installer}" EXIT
 
 GOBIN="${1}"
 CURL="$(which curl)"
-HASHSUM="${2}"
+VERSION="${2}"
+HASHSUM="${3}"
 
 f_sha256() {
   local l_file
@@ -15,16 +16,11 @@ f_sha256() {
   python -sBc "import hashlib;print(hashlib.sha256(open('$l_file','rb').read()).hexdigest())"
 }
 
-get_latest_release() {
-  "${CURL}" --silent "https://api.github.com/repos/$1/releases/latest" | \
-    grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
-}
-VERSION="$(get_latest_release golangci/golangci-lint)"
-
 echo "Downloading golangci-lint ${VERSION} installer ..." >&2
 "${CURL}" -sfL "https://raw.githubusercontent.com/golangci/golangci-lint/${VERSION}/install.sh" > "${installer}"
 
 echo "Checking hashsum ..." >&2
+echo "${installer}"
 [ "${HASHSUM}" = "$(f_sha256 ${installer})" ]
 chmod +x "${installer}"
 

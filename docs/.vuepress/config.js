46ff026589792d1532be45df8771e19b2663cbb6
Denis Fadeev <denis@fadeev.org>
2020-03-02 17:08:52 +0500

    replace Riot with Discord

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -58,7 +58,7 @@ module.exports = {
       }
     },
     footer: {
-      questionsText: "Chat with Cosmos developers on [Riot](https://riot.im/app/#/room/#cosmos-sdk:matrix.org) or reach out on the [SDK Developer Forum](https://forum.cosmos.network/) to learn more.",
+      questionsText: "Chat with Cosmos developers on [Discord](https://discord.gg/cr7N47p) or reach out on the [SDK Developer Forum](https://forum.cosmos.network/) to learn more.",
       logo: "/logo-bw.svg",
       textLink: {
         text: "cosmos.network",

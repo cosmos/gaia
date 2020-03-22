b27f548bf6a7dcc7ce93950921a06a335e01735c
Anil Kumar Kammari <anil@vitwit.com>
2020-03-22 23:16:24 +0530

    Use example prefix

--- types/address.go
+++ types/address.go
@@ -2,15 +2,15 @@ package types
 
 const (
 	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
-	Bech32PrefixAccAddr = "vitwit:"
+	Bech32PrefixAccAddr = "someprefix:"
 	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
-	Bech32PrefixAccPub = "vitwit:pub"
+	Bech32PrefixAccPub = "someprefix:pub"
 	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
-	Bech32PrefixValAddr = "vitwit:valoper"
+	Bech32PrefixValAddr = "someprefix:valoper"
 	// Bech32PrefixValPub defines the Bech32 prefix of a validator's operator public key
-	Bech32PrefixValPub = "vitwit:valoperpub"
+	Bech32PrefixValPub = "someprefix:valoperpub"
 	// Bech32PrefixConsAddr defines the Bech32 prefix of a consensus node address
-	Bech32PrefixConsAddr = "vitwit:valcons"
+	Bech32PrefixConsAddr = "someprefix:valcons"
 	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key
-	Bech32PrefixConsPub = "vitwit:valconspub"
+	Bech32PrefixConsPub = "someprefix:valconspub"
 )

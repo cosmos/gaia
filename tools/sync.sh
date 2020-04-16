03765e9eb3d13f5418b7799813e11ebdad1c6856
Erik Grinaker <erik@interchain.berlin>
2020-04-16 17:21:25 +0200

    update with new state sync options.

--- tools/sync.sh
+++ tools/sync.sh
@@ -8,9 +8,9 @@ read -r HEIGHT HASH <<<$(curl -sSf 'localhost:26657/commit?height=1' | jq -r '"\
 if docker inspect gaiadnode3 >/dev/null; then
   docker stop gaiadnode3
 fi
-gsed -ire 's/^enabled = .*/enabled = true/g' build/node3/gaiad/config/config.toml
+gsed -ire 's/^enable = .*/enable = true/g' build/node3/gaiad/config/config.toml
 gsed -ire 's|^rpc_servers = .*|rpc_servers = "http://192.168.10.2:26657,http://192.168.10.3:26657"|g' build/node3/gaiad/config/config.toml
-gsed -ire 's/^trusted_height = .*/trusted_height = '"$HEIGHT"'/g' build/node3/gaiad/config/config.toml
-gsed -ire 's/^trusted_hash = .*/trusted_hash = "'"$HASH"'"/g' build/node3/gaiad/config/config.toml
-gsed -ire 's/^trusted_period = .*/trusted_period = "1h"/g' build/node3/gaiad/config/config.toml
+gsed -ire 's/^trust_height = .*/trust_height = '"$HEIGHT"'/g' build/node3/gaiad/config/config.toml
+gsed -ire 's/^trust_hash = .*/trust_hash = "'"$HASH"'"/g' build/node3/gaiad/config/config.toml
+gsed -ire 's/^trust_period = .*/trust_period = "1h"/g' build/node3/gaiad/config/config.toml
 docker-compose up gaiadnode3

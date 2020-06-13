4d1013616206d67c1a404dcdd0f45f968e0ae252
Alessio Treglia <alessio@tendermint.com>
2020-06-13 16:19:16 +0200

    Fix ExampleTxSendSize()

--- app/benchmarks/txsize_test.go
+++ app/benchmarks/txsize_test.go
@@ -26,10 +26,10 @@ func ExampleTxSendSize() {
 	priv2 := secp256k1.GenPrivKeySecp256k1([]byte{1})
 	addr2 := sdk.AccAddress(priv2.PubKey().Address())
 	coins := sdk.Coins{sdk.NewCoin("denom", sdk.NewInt(10))}
-	msg1 := bank.MsgMultiSend{
-		Inputs:  []bank.Input{bank.NewInput(addr1, coins)},
-		Outputs: []bank.Output{bank.NewOutput(addr2, coins)},
-	}
+	msg1 := bank.NewMsgMultiSend(
+		[]bank.Input{bank.NewInput(addr1, coins)},
+		[]bank.Output{bank.NewOutput(addr2, coins)},
+	)
 	fee := auth.NewStdFee(gas, coins)
 	signBytes := auth.StdSignBytes("example-chain-ID",
 		1, 1, fee, []sdk.Msg{msg1}, "")

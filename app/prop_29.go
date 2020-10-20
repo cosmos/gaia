cf9376960e8343f90452eb05134c978682d9c7f3
Zaki Manian <zaki@manian.org>
2020-10-19 18:08:40 -0700

    Switch max_num to an integer

--- app/prop_29.go
+++ app/prop_29.go
@@ -506,7 +506,7 @@ func migrateTendermintGenesis(jsonBlob []byte) ([]byte, error) {
 
 	evidenceParams["max_age_duration"] = "172800000000000"
 
-	evidenceParams["max_num"] = "50"
+	evidenceParams["max_num"] = 50
 
 	jsonBlob, err = json.Marshal(jsonObj)
 

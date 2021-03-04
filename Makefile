cosmos/gaia

commit 651f646b5b9a12ad8a40552a5ae883a6abb1336f

Show branch and commit instead of tag when not on release branch (#651)

Makefile
@@ -1,8 +1,15 @@
 #!/usr/bin/make -f
 
-PACKAGES_SIMTEST=$(shell go list ./... | grep '/simulation')
-VERSION := $(shell echo $(shell git describe --tags) | sed 's/^v//')
+BRANCH := $(shell git rev-parse --abbrev-ref HEAD)
 COMMIT := $(shell git log -1 --format='%H')
+
+# ascribe tag only if on a release/ branch, otherwise pick branch name and concatenate commit hash
+VERSION := $(shell echo $(shell git describe --tags) | sed 's/^v//')
+ifeq (, $(findstring release/,$(BRANCH)))
+  VERSION = $(BRANCH)-$(COMMIT)
+endif
+
+PACKAGES_SIMTEST=$(shell go list ./... | grep '/simulation')
 LEDGER_ENABLED ?= true
 SDK_PACK := $(shell go list -m github.com/cosmos/cosmos-sdk | sed  's/ /\@/g')
 DOCKER := $(shell which docker)

commit c12df3a3a012346425a10843d8512d3c3f0c3d48

Makefile: fix version string handling
Most users run builds on tags and not on release
branches. Because of a regression introduced in
651f646b5b9a1, the buildsystem is now assuming that
the opposite is true, therefore it is longer capable
to handle version strings correctly.

Don't use git describe --tags to avoid relying on
non-annotated tags.

BRANCH name detection is ephemeral and unreliable,
and should always be used only as last resort.

Ref: #651

Makefile
@@ -3,10 +3,13 @@
 BRANCH := $(shell git rev-parse --abbrev-ref HEAD)
 COMMIT := $(shell git log -1 --format='%H')
 
-# ascribe tag only if on a release/ branch, otherwise pick branch name and concatenate commit hash
-VERSION := $(shell echo $(shell git describe --tags) | sed 's/^v//')
-ifeq (, $(findstring release/,$(BRANCH)))
-  VERSION = $(BRANCH)-$(COMMIT)
+# don't override user values
+ifeq (,$(VERSION))
+  VERSION := $(shell git describe --exact-match 2>/dev/null)
+  # if VERSION is empty, then populate it with branch's name and raw commit hash
+  ifeq (,$(VERSION))
+    VERSION := $(BRANCH)-$(COMMIT)
+  endif
 endif
 
 PACKAGES_SIMTEST=$(shell go list ./... | grep '/simulation')
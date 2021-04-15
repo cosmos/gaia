ed751de25347c8d3f77233b5e96d4001b2252209
Justin Kilpatrick <justin@althea.net>
2021-04-15 17:10:47 -0400

    Change Althea address prefixes

---
+++ app/addr_prefixes.go
@@ -0,0 +1,40 @@
+package althea
+
+import (
+	"encoding/binary"
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+const (
+	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
+	Bech32PrefixAccAddr = "althea"
+	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
+	Bech32PrefixAccPub = "altheapub"
+	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
+	Bech32PrefixValAddr = "altheavaloper"
+	// Bech32PrefixValPub defines the Bech32 prefix of a validator's operator public key
+	Bech32PrefixValPub = "altheavaloperpub"
+	// Bech32PrefixConsAddr defines the Bech32 prefix of a consensus node address
+	Bech32PrefixConsAddr = "altheavalcons"
+	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key
+	Bech32PrefixConsPub = "altheavalconspub"
+)
+
+func init() {
+	config := sdk.GetConfig()
+	config.SetBech32PrefixForAccount(Bech32PrefixAccAddr, Bech32PrefixAccPub)
+	config.SetBech32PrefixForValidator(Bech32PrefixValAddr, Bech32PrefixValPub)
+	config.SetBech32PrefixForConsensusNode(Bech32PrefixConsAddr, Bech32PrefixConsPub)
+	config.SetAddressVerifier(func(bytes []byte) error {
+		n := len(bytes)
+		if n == sdk.AddrLen {
+			return nil
+		}
+		if n <= binary.MaxVarintLen64+1 {
+			return nil
+		}
+		return fmt.Errorf("unexpected address length %d", n)
+	})
+}

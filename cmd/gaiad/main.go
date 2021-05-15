ff27a9edff421688f90673e85a0b1cfdcb00b4ec
Zaki Manian <zaki@manian.org>
2021-05-14 21:34:03 -0700

    Add agoric chain config

--- cmd/gaiad/main.go
+++ cmd/gaiad/main.go
@@ -5,12 +5,48 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/server"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	app "github.com/cosmos/gaia/v5/app"
 	"github.com/cosmos/gaia/v5/cmd/gaiad/cmd"
 )
 
+const (
+	// Bech32MainPrefix defines the Bech32 prefix used by all types
+	Bech32MainPrefix = "agoric"
+
+	// CoinType is used in the slip44 HD key derivation path.
+	// https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+	CoinType = 564
+
+	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
+	Bech32PrefixAccAddr = Bech32MainPrefix
+	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
+	Bech32PrefixAccPub = Bech32MainPrefix + sdk.PrefixPublic
+	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
+	Bech32PrefixValAddr = Bech32MainPrefix + sdk.PrefixValidator + sdk.PrefixOperator
+	// Bech32PrefixValPub defines the Bech32 prefix of a validator's operator public key
+	Bech32PrefixValPub = Bech32MainPrefix + sdk.PrefixValidator + sdk.PrefixOperator + sdk.PrefixPublic
+	// Bech32PrefixConsAddr defines the Bech32 prefix of a consensus node address
+	Bech32PrefixConsAddr = Bech32MainPrefix + sdk.PrefixValidator + sdk.PrefixConsensus
+	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key
+	Bech32PrefixConsPub = Bech32MainPrefix + sdk.PrefixValidator + sdk.PrefixConsensus + sdk.PrefixPublic
+)
+
+// SetConfigDefaults sets the appropriate parameters for the Agoric chain.
+func SetConfigDefaults(config *sdk.Config) {
+	config.SetCoinType(CoinType)
+	config.SetBech32PrefixForAccount(Bech32PrefixAccAddr, Bech32PrefixAccPub)
+	config.SetBech32PrefixForValidator(Bech32PrefixValAddr, Bech32PrefixValPub)
+	config.SetBech32PrefixForConsensusNode(Bech32PrefixConsAddr, Bech32PrefixConsPub)
+}
+
 func main() {
+
+	config := sdk.GetConfig()
+	SetConfigDefaults(config)
+	config.Seal()
+
 	rootCmd, _ := cmd.NewRootCmd()
 
 	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {

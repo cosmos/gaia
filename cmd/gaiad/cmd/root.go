04f589a3ad42a9bf59f0111437b01f892ebfcb9a
Shahan Khatchadourian <shahan.k.code@gmail.com>
2021-06-28 10:06:00 -0400

    Cherrypick dex v5 go.sum (#894)
    * Bump sdk, tendermint version of liquidity module (#891)
    
    * bump sdk, tendermint version of liquidity module
    
    * exclude LegacyAmino cases of lint
    
    * Update gaia module version to v5 (#892)
    
    * Update changelog
    
    * Update gaia package to v5
    
    * Update go.sum (#893)
    
    Co-authored-by: dongsam <dongsamb@gmail.com>

--- cmd/gaiad/cmd/root.go
+++ cmd/gaiad/cmd/root.go
@@ -29,8 +29,8 @@ import (
 	"github.com/tendermint/tendermint/libs/log"
 	dbm "github.com/tendermint/tm-db"
 
-	gaia "github.com/cosmos/gaia/v4/app"
-	"github.com/cosmos/gaia/v4/app/params"
+	gaia "github.com/cosmos/gaia/v5/app"
+	"github.com/cosmos/gaia/v5/app/params"
 )
 
 // NewRootCmd creates a new root command for simd. It is called once in the

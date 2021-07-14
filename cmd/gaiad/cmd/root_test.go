0fb0dfd467495ec16e8d3f4b613fb9d28836d939
billy rennekamp <billy.rennekamp@gmail.com>
2021-07-14 19:09:41 +0200

    Merge pull request #907: add back the config command (#908)
    * fix: add back the config command and run linting
    
    * test: increase code coverage

---
+++ cmd/gaiad/cmd/root_test.go
@@ -0,0 +1,23 @@
+package cmd_test
+
+import (
+	"testing"
+
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	app "github.com/cosmos/gaia/v5/app"
+	"github.com/cosmos/gaia/v5/cmd/gaiad/cmd"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRootCmdConfig(t *testing.T) {
+
+	rootCmd, _ := cmd.NewRootCmd()
+	rootCmd.SetArgs([]string{
+		"config",          // Test the config cmd
+		"keyring-backend", // key
+		"test",            // value
+	})
+
+	require.NoError(t, svrcmd.Execute(rootCmd, app.DefaultNodeHome))
+}

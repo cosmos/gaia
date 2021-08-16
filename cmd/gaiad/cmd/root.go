2d186b48b148c55cc63ac2216cf066a4e14a3e63
yaruwang <yaruwang.materials@gmail.com>
2021-08-16 18:33:35 +0200

    gaia v5 to v6

--- cmd/gaiad/cmd/root.go
+++ cmd/gaiad/cmd/root.go
@@ -34,8 +34,8 @@ import (
 	feegrantcli "github.com/cosmos/cosmos-sdk/x/feegrant/client/cli"
 	genutilcli "github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
 
-	gaia "github.com/cosmos/gaia/v5/app"
-	"github.com/cosmos/gaia/v5/app/params"
+	gaia "github.com/cosmos/gaia/v6/app"
+	"github.com/cosmos/gaia/v6/app/params"
 )
 
 // NewRootCmd creates a new root command for simd. It is called once in the

67529f59d346fc5095130a11bcc3b43e9f61dbc2
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-08-16 12:27:56 -0400

    app updates

--- app/app.go
+++ app/app.go
@@ -548,8 +548,9 @@ func NewGaiaApp(
 	app.SetBeginBlocker(app.BeginBlocker)
 	app.SetEndBlocker(app.EndBlocker)
 
+	// TODO: Verify the upgrade name.
 	app.UpgradeKeeper.SetUpgradeHandler(
-		"gaia-v6.0.0", // TODO: Verify the upgrade name.
+		"gaia-v6.0.0",
 		func(ctx sdk.Context, _ upgradetypes.Plan, _ module.VersionMap) (module.VersionMap, error) {
 			// TODO: Verify these parameters are correct and intended.
 			app.IBCKeeper.ConnectionKeeper.SetParams(ctx, ibcconnectiontypes.DefaultParams())
@@ -559,7 +560,8 @@ func NewGaiaApp(
 			}
 
 			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
-		})
+		},
+	)
 
 	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
 	if err != nil {

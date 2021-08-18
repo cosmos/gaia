a3135c43b184e7411db6ee417cc8040d710acdec
billy rennekamp <billy.rennekamp@gmail.com>
2021-08-18 18:58:46 +0200

    add module versions

--- app/app.go
+++ app/app.go
@@ -555,7 +555,22 @@ func NewGaiaApp(
 			app.IBCKeeper.ConnectionKeeper.SetParams(ctx, ibcconnectiontypes.DefaultParams())
 
 			fromVM := map[string]uint64{
-				"ibc": 1,
+				"auth":         1,
+				"bank":         1,
+				"capability":   1,
+				"crisis":       1,
+				"distribution": 1,
+				"evidence":     1,
+				"gov":          1,
+				"mint":         1,
+				"params":       1,
+				"slashing":     1,
+				"staking":      1,
+				"upgrade":      1,
+				"vesting":      1,
+				"ibc":          1,
+				"genutil":      1,
+				"transfer":     1,
 			}
 
 			return app.mm.RunMigrations(ctx, app.configurator, fromVM)

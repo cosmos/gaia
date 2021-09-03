51f43db857695bb85c6a05143f2cabdc1f56ea71
Jacob Gadikian <jacobgadikian@gmail.com>
2021-09-03 10:56:29 +0700

    Update statesync.sh

--- contrib/statesync.sh
+++ contrib/statesync.sh
@@ -1,67 +1,36 @@
-
 #!/bin/bash
-# Based on the work of Joe (Chorus-One) for Microtick - https://github.com/microtick/bounties/tree/main/statesync
-# You need config in two peers (avoid seed servers) this values in app.toml:
-#     [state-sync]
-#     snapshot-interval = 1000
-#     snapshot-keep-recent = 10
-# Pruning should be fine tuned also, for this testings is set to nothing
-#     pruning = "nothing"
-
+# microtick and bitcanna contributed significantly here.
 set -e
 
-# Change for your custom chain
+export GOPATH=~/go
+export PATH=$PATH:~/go/bin
+go install ./...
 
-  
-NODE1_IP="95.217.196.54"
-RPC1="http://$NODE1_IP"
-P2P_PORT1=2010
-RPC_PORT1=2011
 
-NODE2_IP="95.217.196.54"
-RPC2="http://$NODE2_IP"
-P2P_PORT2=2010
-RPC_PORT2=2011
+# MAKE HOME FOLDER AND GET GENESIS
+osmosisd init test 
+wget -O ~/.gaia/config/genesis.json https://cloudflare-ipfs.com/ipfs/Qmc54DreioPpPDUdJW6bBTYUKepmcPsscfqsfFcFmTaVig
 
-  #If you want to use a third StateSync Server... 
-  #DOMAIN_3=seed1.bitcanna.io     # If you want to use domain names 
-  #NODE3_IP=$(dig $DOMAIN_1 +short
-  #RPC3="http://$NODE3_IP"
-  #RPC_PORT3=26657
-  #P2P_PORT3=26656
+INTERVAL=1500
 
-INTERVAL=1000
+# GET TRUST HASH AND TRUST HEIGHT
 
-LATEST_HEIGHT=$(curl -s $RPC1:$RPC_PORT1/block | jq -r .result.block.header.height);
+LATEST_HEIGHT=$(curl -s 162.55.132.230:2011/block | jq -r .result.block.header.height);
 BLOCK_HEIGHT=$(($LATEST_HEIGHT-$INTERVAL)) 
-  
-if [ $BLOCK_HEIGHT -eq 0 ]; then
-    echo "Error: Cannot state sync to block 0; Latest block is $LATEST_HEIGHT and must be at least $INTERVAL; wait a few blocks!"
-    exit 1
-fi
-
-TRUST_HASH=$(curl -s "$RPC1:$RPC_PORT1/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
-    if [ "$TRUST_HASH" == "null" ]; then
-    echo "Error: Cannot find block hash. This shouldn't happen :/"
-exit 1
-fi
-
-NODE1_ID=$(curl -s "$RPC1:$RPC_PORT1/status" | jq -r .result.node_info.id)
-NODE2_ID=$(curl -s "$RPC2:$RPC_PORT2/status" | jq -r .result.node_info.id)
-#NODE3_ID=$(curl -s "$RPC3:$RPC_PORT3/status" | jq -r .result.node_info.id)
-
-echo "NODE ONE: $NODE1_ID@$NODE1_IP:$P2P_PORT1"
-echo "NODE TWO: $NODE2_ID@$NODE2_IP:$P2P_PORT2"
+TRUST_HASH=$(curl -s "162.55.132.230:2011/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
 
 
+# TELL USER WHAT WE ARE DOING
+echo "TRUST HEIGHT: $BLOCK_HEIGHT"
+echo "TRUST HASH: $TRUST_HASH"
 
-sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"http://$NODE1_IP:$RPC_PORT1,http://$NODE2_IP:$RPC_PORT2\"| ; \
-s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
-s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
-s|^(persistent_peers[[:space:]]+=[[:space:]]+).*$|\1\"${NODE1_ID}@${NODE1_IP}:${P2P_PORT1},${NODE2_ID}@${NODE2_IP}:${P2P_PORT2}\"|" $HOME/.gaia/config/config.toml
 
- 
+# export state sync vars
+export GAIAD_STATESYNC_ENABLE=true
+export GAIAD_STATESYNC_RPC_SERVERS="162.55.132.230:2011,162.55.132.230:2011"
+export GAIAD_STATESYNC_TRUST_HEIGHT=$BLOCK_HEIGHT
+export GAIAD_STATESYNC_TRUST_HASH=$TRUST_HASH
+export GAIAD_P2P_PERSISTENT_PEERS="7f317a82192462dcf0dca48111afcce895cf68d0@162.55.132.230:2010"
 
 gaiad unsafe-reset-all
 gaiad start

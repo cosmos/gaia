5f45540821140c8666cb22f06dcd5355266de5d9
Jacob Gadikian <jacobgadikian@gmail.com>
2021-11-17 04:03:08 +0700

    Update statesync.bash
    clean up addresses, add seeds, use make targets

--- contrib/statesync.bash
+++ contrib/statesync.bash
@@ -8,8 +8,7 @@ export PATH=$PATH:~/go/bin
 
 
 # Install Gaia
-go install ./...
-
+make install
 
 # MAKE HOME FOLDER AND GET GENESIS
 gaiad init test 
@@ -19,9 +18,9 @@ INTERVAL=1000
 
 # GET TRUST HASH AND TRUST HEIGHT
 
-LATEST_HEIGHT=$(curl -s 162.55.132.230:2011/block | jq -r .result.block.header.height);
+LATEST_HEIGHT=$(curl -s https://cosmoshub-4.technofractal.com | jq -r .result.block.header.height);
 BLOCK_HEIGHT=$(($LATEST_HEIGHT-$INTERVAL)) 
-TRUST_HASH=$(curl -s "162.55.132.230:2011/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+TRUST_HASH=$(curl -s "https://cosmoshub-4.technofractal.com/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
 
 
 # TELL USER WHAT WE ARE DOING
@@ -36,5 +35,6 @@ export GAIAD_STATESYNC_RPC_SERVERS="https://cosmoshub-4.technofractal.com:443,ht
 export GAIAD_STATESYNC_TRUST_HEIGHT=$BLOCK_HEIGHT
 export GAIAD_STATESYNC_TRUST_HASH=$TRUST_HASH
 export GAIAD_P2P_PERSISTENT_PEERS="2bb31c07148a689f0b2dd363e17631993eca1020@162.55.132.230:2010"
+export GAIAD_P2P_SEEDS="bf8328b66dceb4987e5cd94430af66045e59899f@public-seed.cosmos.vitwit.com:26656,cfd785a4224c7940e9a10f6c1ab24c343e923bec@164.68.107.188:26656,d72b3011ed46d783e369fdf8ae2055b99a1e5074@173.249.50.25:26656,ba3bacc714817218562f743178228f23678b2873@public-seed-node.cosmoshub.certus.one:26656,3c7cad4154967a294b3ba1cc752e40e8779640ad@84.201.128.115:26656,366ac852255c3ac8de17e11ae9ec814b8c68bddb@51.15.94.196:26656"
 
 gaiad start --x-crisis-skip-assert-invariants

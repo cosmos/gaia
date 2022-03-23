92d08b75e57179bdd44301c77e7ca38e77cefa9b
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-03-23 12:54:28 -0400

    updates

--- app/params/config.go
+++ app/params/config.go
@@ -13,14 +13,17 @@ var (
 	// CustomConfigTemplate defines Gaia's custom application configuration TOML
 	// template. It extends the core SDK template.
 	CustomConfigTemplate = serverconfig.DefaultConfigTemplate + `
-	###############################################################################
-	###                        Custom Gaia Configuration                        ###
-	###############################################################################
-	
-	# bypass-min-fee-msg-types defines custom message types the operator may set that
-	# will bypass minimum fee checks during CheckTx.
-	bypass-min-fee-msg-types = {{ .BypassMinFeeMsgTypes }}
-	`
+###############################################################################
+###                        Custom Gaia Configuration                        ###
+###############################################################################
+
+# bypass-min-fee-msg-types defines custom message types the operator may set that
+# will bypass minimum fee checks during CheckTx.
+#
+# Example:
+# ["/ibc.core.channel.v1.MsgRecvPacket", "/ibc.core.channel.v1.MsgAcknowledgement", ...]
+bypass-min-fee-msg-types = {{ .BypassMinFeeMsgTypes }}
+`
 )
 
 // CustomAppConfig defines Gaia's custom application configuration.

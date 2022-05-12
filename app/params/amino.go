c2968aabcf97289454ff7ddced641a2be74b2a63
Yaru Wang <yaru@interchain.io>
2022-05-12 19:49:47 +0200

    fix: keys list

--- app/params/amino.go
+++ app/params/amino.go
@@ -3,17 +3,22 @@
 
 package params
 
-// This function should be used only internally (in the SDK).
-// App user should'nt create new codecs - use the app.AppCodec instead.
-// [DEPRECATED]
-// func MakeTestEncodingConfig() EncodingConfig {
-// 	cdc := codec.NewLegacyAmino()
-// 	interfaceRegistry := types.NewInterfaceRegistry()
-// 	marshaler := codec.NewAminoCodec(cdc)
-// 	return EncodingConfig{
-// 		InterfaceRegistry: interfaceRegistry,
-// 		Marshaler:         marshaler,
-// 		TxConfig:          legacytx.StdTxConfig{Cdc: cdc},
-// 		Amino:             cdc,
-// 	}
-// }
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/migrations/legacytx"
+)
+
+func MakeTestEncodingConfig() EncodingConfig {
+	cdc := codec.NewLegacyAmino()
+	interfaceRegistry := cdctypes.NewInterfaceRegistry()
+	marshaler := codec.NewAminoCodec(cdc)
+
+	return EncodingConfig{
+		InterfaceRegistry: interfaceRegistry,
+		Marshaler:         marshaler,
+		TxConfig:          legacytx.StdTxConfig{Cdc: cdc},
+		Amino:             cdc,
+	}
+}

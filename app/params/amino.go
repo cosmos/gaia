7e20e417adeb5db8242626300bf8c3f84b6d8f13
billy rennekamp <billy.rennekamp@gmail.com>
2022-05-13 12:09:19 +0200

    nits

--- app/params/amino.go
+++ app/params/amino.go
@@ -3,17 +3,23 @@
 
 package params
 
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/legacy/legacytx"
+)
+
 // This function should be used only internally (in the SDK).
 // App user should'nt create new codecs - use the app.AppCodec instead.
 // [DEPRECATED]
-// func MakeTestEncodingConfig() EncodingConfig {
-// 	cdc := codec.NewLegacyAmino()
-// 	interfaceRegistry := types.NewInterfaceRegistry()
-// 	marshaler := codec.NewAminoCodec(cdc)
-// 	return EncodingConfig{
-// 		InterfaceRegistry: interfaceRegistry,
-// 		Marshaler:         marshaler,
-// 		TxConfig:          legacytx.StdTxConfig{Cdc: cdc},
-// 		Amino:             cdc,
-// 	}
-// }
+func MakeTestEncodingConfig() EncodingConfig {
+	cdc := codec.NewLegacyAmino()
+	interfaceRegistry := types.NewInterfaceRegistry()
+	marshaler := codec.NewAminoCodec(cdc)
+	return EncodingConfig{
+		InterfaceRegistry: interfaceRegistry,
+		Marshaler:         marshaler,
+		TxConfig:          legacytx.StdTxConfig{Cdc: cdc},
+		Amino:             cdc,
+	}
+}

0ee373ef741ae726d18a3a2850f51ac2266d74d9
lg <lauren@interchain.io>
2022-05-14 19:42:02 +0200

    Replace commented out test with Skip

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -2,52 +2,55 @@ package e2e
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// func (s *IntegrationTestSuite) TestIBCTokenTransfer() {
-// 	var ibcStakeDenom string
+func (s *IntegrationTestSuite) TestIBCTokenTransfer() {
+	// TODO: Remove skip once IBC is reintegrated
+	s.T().Skip()
+	var ibcStakeDenom string
 
-// 	s.Run("send_photon_to_chainB", func() {
+	s.Run("send_photon_to_chainB", func() {
 
-// 		// require the recipient account receives the IBC tokens (IBC packets ACKd)
-// 		var (
-// 			balances sdk.Coins
-// 			err      error
-// 		)
+		// require the recipient account receives the IBC tokens (IBC packets ACKd)
+		var (
+			balances sdk.Coins
+			err      error
+		)
 
-// 		address, err := s.chainB.validators[0].keyInfo.GetAddress()
-// 		s.Require().NoError(err)
-// 		recipient := address.String()
-// 		token := sdk.NewInt64Coin(photonDenom, 3300000000) // 3,300photon
-// 		s.sendIBC(s.chainA.id, s.chainB.id, recipient, token)
+		address, err := s.chainB.validators[0].keyInfo.GetAddress()
+		s.Require().NoError(err)
+		recipient := address.String()
+		token := sdk.NewInt64Coin(photonDenom, 3300000000) // 3,300photon
+		s.sendIBC(s.chainA.id, s.chainB.id, recipient, token)
 
-// 		chainBAPIEndpoint := fmt.Sprintf("http://%s", s.valResources[s.chainB.id][0].GetHostPort("1317/tcp"))
+		chainBAPIEndpoint := fmt.Sprintf("http://%s", s.valResources[s.chainB.id][0].GetHostPort("1317/tcp"))
 
-// 		s.Require().Eventually(
-// 			func() bool {
-// 				balances, err = queryGaiaAllBalances(chainBAPIEndpoint, recipient)
-// 				s.Require().NoError(err)
+		s.Require().Eventually(
+			func() bool {
+				balances, err = queryGaiaAllBalances(chainBAPIEndpoint, recipient)
+				s.Require().NoError(err)
 
-// 				return balances.Len() == 3
-// 			},
-// 			time.Minute,
-// 			5*time.Second,
-// 		)
+				return balances.Len() == 3
+			},
+			time.Minute,
+			5*time.Second,
+		)
 
-// 		for _, c := range balances {
-// 			if strings.Contains(c.Denom, "ibc/") {
-// 				ibcStakeDenom = c.Denom
-// 				s.Require().Equal(token.Amount.Int64(), c.Amount.Int64())
-// 				break
-// 			}
-// 		}
+		for _, c := range balances {
+			if strings.Contains(c.Denom, "ibc/") {
+				ibcStakeDenom = c.Denom
+				s.Require().Equal(token.Amount.Int64(), c.Amount.Int64())
+				break
+			}
+		}
 
-// 		s.Require().NotEmpty(ibcStakeDenom)
-// 	})
-// }
+		s.Require().NotEmpty(ibcStakeDenom)
+	})
+}
 
 func (s *IntegrationTestSuite) TestBankTokenTransfer() {
 	s.Run("send_photon_between_accounts", func() {

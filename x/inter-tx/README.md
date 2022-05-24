ed1f43c6db1df1841099906caf7de7ed262b2eb1
Yaru Wang <yaru@interchain.io>
2022-05-24 18:53:09 +0200

    add ica module

---
+++ x/inter-tx/README.md
@@ -0,0 +1,4 @@
+# Mauth module
+
+Mauth module is copied from the [interchain-accounts-demo](https://github.com/cosmos/interchain-accounts-demo) respository
+with minor modifications. All credits and big thanks go to the original authors.
---
+++ x/inter-tx/keeper/msg_server_test.go
@@ -0,0 +1,90 @@
+package keeper_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	icatypes "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts/types"
+	channeltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
+	ibctesting "github.com/cosmos/ibc-go/v3/testing"
+
+	"github.com/cosmos/gaia/v8/x/inter-tx/keeper"
+	"github.com/cosmos/gaia/v8/x/inter-tx/types"
+)
+
+func (suite *KeeperTestSuite) TestRegisterInterchainAccount() {
+	var (
+		owner string
+		path  *ibctesting.Path
+	)
+
+	testCases := []struct {
+		name     string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"success", func() {}, true,
+		},
+		{
+			"port is already bound",
+			func() {
+				suite.GetICAApp(suite.chainA).IBCKeeper.PortKeeper.BindPort(suite.chainA.GetContext(), TestPortID)
+			},
+			false,
+		},
+		{
+			"fails to generate port-id",
+			func() {
+				owner = ""
+			},
+			false,
+		},
+		{
+			"MsgChanOpenInit fails - channel is already active",
+			func() {
+				portID, err := icatypes.NewControllerPortID(owner)
+				suite.Require().NoError(err)
+
+				channel := channeltypes.NewChannel(
+					channeltypes.OPEN,
+					channeltypes.ORDERED,
+					channeltypes.NewCounterparty(path.EndpointB.ChannelConfig.PortID, path.EndpointB.ChannelID),
+					[]string{path.EndpointA.ConnectionID},
+					path.EndpointA.ChannelConfig.Version,
+				)
+				suite.GetICAApp(suite.chainA).IBCKeeper.ChannelKeeper.SetChannel(suite.chainA.GetContext(), portID, ibctesting.FirstChannelID, channel)
+
+				suite.GetICAApp(suite.chainA).ICAControllerKeeper.SetActiveChannelID(suite.chainA.GetContext(), ibctesting.FirstConnectionID, portID, ibctesting.FirstChannelID)
+			},
+			false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		suite.Run(tc.name, func() {
+			suite.SetupTest()
+
+			owner = TestOwnerAddress // must be explicitly changed
+
+			path = NewICAPath(suite.chainA, suite.chainB)
+			suite.coordinator.SetupConnections(path)
+
+			tc.malleate() // malleate mutates test data
+
+			msgSrv := keeper.NewMsgServerImpl(suite.GetICAApp(suite.chainA).InterTxKeeper)
+			msg := types.NewMsgRegisterAccount(owner, path.EndpointA.ConnectionID)
+
+			res, err := msgSrv.RegisterAccount(sdk.WrapSDKContext(suite.chainA.GetContext()), msg)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+				suite.Require().NotNil(res)
+			} else {
+				suite.Require().Error(err)
+				suite.Require().Nil(res)
+			}
+
+		})
+	}
+}

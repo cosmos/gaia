a09c8255aec391fd8ee8f7dccc43d9207ebfbd52
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-06-14 16:12:27 -0400

    updates

---
+++ app/app_test.go
@@ -0,0 +1,36 @@
+package gaia_test
+
+import (
+	"testing"
+
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/libs/log"
+	db "github.com/tendermint/tm-db"
+
+	gaia "github.com/cosmos/gaia/v7/app"
+)
+
+type EmptyAppOptions struct{}
+
+func (ao EmptyAppOptions) Get(o string) interface{} {
+	return nil
+}
+
+func TestGaiaApp_BlockedModuleAccountAddrs(t *testing.T) {
+	app := gaia.NewGaiaApp(
+		log.NewNopLogger(),
+		db.NewMemDB(),
+		nil,
+		true,
+		map[int64]bool{},
+		gaia.DefaultNodeHome,
+		0,
+		gaia.MakeEncodingConfig(),
+		EmptyAppOptions{},
+	)
+	blockedAddrs := app.BlockedModuleAccountAddrs()
+
+	require.NotContains(t, blockedAddrs, authtypes.NewModuleAddress(govtypes.ModuleName).String())
+}

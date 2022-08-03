47bb2cc89d3f5ae0b310fea1f4e8ca20a0595e5f
billy rennekamp <billy.rennekamp@gmail.com>
2022-08-03 17:00:32 +0200

    ensure correct endpoint

--- tests/e2e/e2e_util_test.go
+++ tests/e2e/e2e_util_test.go
@@ -357,6 +357,7 @@ func (s *IntegrationTestSuite) executeGaiaTxCommand(ctx context.Context, c *chai
 		time.Second,
 		"tx returned a non-zero code; stdout: %s, stderr: %s", outBuf.String(), errBuf.String(),
 	)
+	endpoint = fmt.Sprintf("http://%s", s.valResources[s.chainA.id][0].GetHostPort("1317/tcp"))
 	s.Require().Eventually(
 		func() bool {
 			return queryGaiaTx(endpoint, txResp.TxHash) == nil

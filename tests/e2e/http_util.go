8e49b67cd585e1cdf53ff906040a23410bf0f751
lg <8335464+glnro@users.noreply.github.com>
2022-09-13 09:05:01 +0200

    test: E2E Distribution module tests (#1692)
    * E2E Distribution tests
    
    * Make requested changes
    
    * Remove comments
    
    * Update fn name
    
    * Update tests/e2e/e2e_distribution_test.go
    
    Co-authored-by: Danilo Pantani <danpantani@gmail.com>
    
    * Update tests/e2e/e2e_distribution_test.go
    
    Co-authored-by: Danilo Pantani <danpantani@gmail.com>
    
    * Update tests/e2e/e2e_util_test.go
    
    Co-authored-by: Danilo Pantani <danpantani@gmail.com>
    
    * Update tests/e2e/e2e_test.go
    
    Co-authored-by: Danilo Pantani <danpantani@gmail.com>
    
    * Update tests/e2e/e2e_util_test.go
    
    Co-authored-by: Danilo Pantani <danpantani@gmail.com>
    
    * Remove debug query function
    
    Co-authored-by: Danilo Pantani <danpantani@gmail.com>

---
+++ tests/e2e/http_util.go
@@ -0,0 +1,22 @@
+package e2e
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func httpGet(endpoint string) ([]byte, error) {
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute HTTP request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}

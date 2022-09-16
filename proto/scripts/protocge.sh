beb509377d1e2c9e56c140af0aeb89b35d4c4146
Yaru Wang <yaru@interchain.io>
2022-09-16 15:55:01 +0200

    chore: delete extra protogen.sh

--- proto/scripts/protocge.sh
+++ proto/scripts/protocge.sh
@@ -13,8 +13,6 @@ protoc_gen_gocosmos() {
 
 protoc_gen_gocosmos
 
-# Get the path of the cosmos-sdk repo from go/pkg/mod
-# cosmos_sdk_dir=$(go list -f '{{ .Dir }}' -m github.com/cosmos/cosmos-sdk)
 proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   protoc \
@@ -23,8 +21,18 @@ for dir in $proto_dirs; do
   --gocosmos_out=plugins=interfacetype+grpc,\
 Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
   --grpc-gateway_out=logtostderr=true:. \
-    $(find "${dir}" -maxdepth 1 -name '*.proto')
+  $(find "${dir}" -maxdepth 1 -name '*.proto')
+
 done
 
-cp -r github.com/cosmos/gaia/x/* x/
+# command to generate docs using protoc-gen-doc
+protoc \
+-I "proto" \
+-I "third_party/proto" \
+--doc_out=../../docs/proto \
+--doc_opt=../../docs/proto/protodoc-markdown.tmpl,proto-docs.md \
+$(find "$(pwd)/proto" -maxdepth 5 -name '*.proto')
+
+# move proto files to the right places
+cp -r github.com/cosmos/gaia/x/* ./
 rm -rf github.com

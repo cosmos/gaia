19be7307f210e9c2cc6678c4a2e80328a0162967
lg <lauren@interchain.io>
2022-10-10 20:12:47 +0200

    wip

--- app/upgrades/v8/constants.go
+++ app/upgrades/v8/constants.go
@@ -2,6 +2,8 @@ package v8
 
 import (
 	store "github.com/cosmos/cosmos-sdk/store/types"
+	group "github.com/cosmos/cosmos-sdk/x/group"
+
 	"github.com/cosmos/gaia/v8/app/upgrades"
 	"github.com/cosmos/gaia/v8/x/globalfee"
 )
@@ -15,6 +17,9 @@ var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateUpgradeHandler,
 	StoreUpgrades: store.StoreUpgrades{
-		Added: []string{globalfee.ModuleName},
+		Added: []string{
+			group.ModuleName,
+			globalfee.ModuleName,
+		},
 	},
 }
--- app/upgrades/v8/upgrades.go
+++ app/upgrades/v8/upgrades.go
@@ -5,8 +5,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	"github.com/cosmos/gaia/v8/app/keepers"
-	"github.com/cosmos/gaia/v8/x/globalfee"
-	globalfeetypes "github.com/cosmos/gaia/v8/x/globalfee/types"
 )
 
 func CreateUpgradeHandler(
@@ -15,15 +13,6 @@ func CreateUpgradeHandler(
 	keepers *keepers.AppKeepers,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
-		// globalfee params
-		globalFeeParams := globalfeetypes.DefaultParams()
-		globalFeeModule, err := mm.Modules[globalfee.ModuleName].(globalfee.AppModule)
-		if err {
-			panic("mm.Modules[globalfee.ModuleName] is not of type globalfee.AppModule")
-		}
-		globalFeeModule.InitModule(ctx, globalFeeParams)
-		// override versions for _new_ modules as to not skip InitGenesis...only for existing module
-		//vm[group.ModuleName] = 0
 
 		//controllerParams := icacontrollertypes.Params{}
 		//// allowmessages = [*]

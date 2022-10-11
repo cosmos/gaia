afece90e0a5efbbd71d49da9711e7b32cdea3727
Danilo Pantani <danpantani@gmail.com>
2022-10-11 02:26:26 -0300

    check if the atom denom exists

--- app/upgrades/v8/upgrades.go
+++ app/upgrades/v8/upgrades.go
@@ -1,10 +1,12 @@
 package v8
 
 import (
+	"errors"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
 	"github.com/cosmos/gaia/v8/app/keepers"
 )
 
@@ -16,20 +18,15 @@ func CreateUpgradeHandler(
 	return func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		ctx.Logger().Info("start to run module migrations...")
 
-		// Add Bank denom metadata
-		keepers.BankKeeper.SetDenomMetaData(ctx, banktypes.Metadata{
-			Name:        "Cosmos Hub Atom",
-			Symbol:      "ATOM",
-			Description: "The native staking token of the Cosmos Hub.",
-			DenomUnits: []*banktypes.DenomUnit{
-				{"uatom", uint32(0), []string{"microatom"}},
-				{"matom", uint32(3), []string{"milliatom"}},
-				{"atom", uint32(6), nil},
-			},
-			Base:    "uatom",
-			Display: "atom",
-		})
-		
+		// Add atom name and symbol into the bank keeper
+		atomMetaData, found := keepers.BankKeeper.GetDenomMetaData(ctx, "uatom")
+		if !found {
+			return nil, errors.New("atom denom not found")
+		}
+		atomMetaData.Name = "Cosmos Hub Atom"
+		atomMetaData.Symbol = "ATOM"
+		keepers.BankKeeper.SetDenomMetaData(ctx, atomMetaData)
+
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
 }

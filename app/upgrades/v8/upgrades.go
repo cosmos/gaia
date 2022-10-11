89d63d60ee62b91bf1fdc6cb437d9aa3099ed019
Danilo Pantani <danpantani@gmail.com>
2022-10-10 21:18:35 -0300

    add bank denom metadata

--- app/upgrades/v8/upgrades.go
+++ app/upgrades/v8/upgrades.go
@@ -3,8 +3,8 @@ package v8
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
-
 	"github.com/cosmos/gaia/v8/app/keepers"
 )
 
@@ -16,6 +16,20 @@ func CreateUpgradeHandler(
 	return func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		ctx.Logger().Info("start to run module migrations...")
 
+		// Add Bank denom metadata
+		keepers.BankKeeper.SetDenomMetaData(ctx, banktypes.Metadata{
+			Name:        "Cosmos Hub Atom",
+			Symbol:      "ATOM",
+			Description: "The native staking token of the Cosmos Hub.",
+			DenomUnits: []*banktypes.DenomUnit{
+				{"uatom", uint32(0), []string{"microatom"}},
+				{"matom", uint32(3), []string{"milliatom"}},
+				{"atom", uint32(6), nil},
+			},
+			Base:    "uatom",
+			Display: "atom",
+		})
+		
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
 }

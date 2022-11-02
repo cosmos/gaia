97c1c9ea5a255e1ed57b10ae411d4c0a7859c97e
billy rennekamp <billy.rennekamp@gmail.com>
2022-11-02 03:35:54 -0500

    chore: bump sdk to v0.46.4 (#1863)
    * chore: bump sdk
    
    * rename flag

--- cmd/gaiad/cmd/root.go
+++ cmd/gaiad/cmd/root.go
@@ -247,7 +247,7 @@ func (ac appCreator) newApp(
 		baseapp.SetIndexEvents(cast.ToStringSlice(appOpts.Get(server.FlagIndexEvents))),
 		baseapp.SetSnapshot(snapshotStore, snapshotOptions),
 		baseapp.SetIAVLCacheSize(cast.ToInt(appOpts.Get(server.FlagIAVLCacheSize))),
-		baseapp.SetIAVLDisableFastNode(cast.ToBool(appOpts.Get(server.FlagIAVLFastNode))),
+		baseapp.SetIAVLDisableFastNode(cast.ToBool(appOpts.Get(server.FlagDisableIAVLFastNode))),
 	)
 }
 

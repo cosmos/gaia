56b2eb0b73675a1bb2e26e9f6fcba59924eee23c
Yaru Wang <yaru@interchain.io>
2022-11-02 13:09:42 +0100

    use original test

--- x/globalfee/querier_test.go
+++ x/globalfee/querier_test.go
@@ -1,88 +1,56 @@
-package globalfee_test
+package globalfee
 
 import (
-	"fmt"
 	"testing"
 
-	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/gaia/v8/x/globalfee"
-	"github.com/cosmos/gaia/v8/x/globalfee/types"
-	"github.com/stretchr/testify/suite"
-	tmrand "github.com/tendermint/tendermint/libs/rand"
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
-	"github.com/cosmos/gaia/v8/app"
-	gaiahelpers "github.com/cosmos/gaia/v8/app/helpers"
+	"github.com/cosmos/gaia/v8/x/globalfee/types"
 )
 
-type testSuite struct {
-	suite.Suite
-
-	app         *gaia.GaiaApp
-	ctx         sdk.Context
-	queryClient globalfee.GrpcQuerier
-}
-
-func TestGRPCQueryMinimumGasPricesSuite(t *testing.T) {
-	suite.Run(t, new(testSuite))
-}
-
-func (s *testSuite) TestGRPCQueryMinimumGasPrices() {
-	s.setup()
-
-	// MinimumGasPrices: empty coins
-	emptyCoins := sdk.DecCoins{}
-	globalFeeParams := types.Params{
-		MinimumGasPrices: emptyCoins,
+func TestQueryMinimumGasPrices(t *testing.T) {
+	specs := map[string]struct {
+		setupStore func(ctx sdk.Context, s paramtypes.Subspace)
+		expMin     sdk.DecCoins
+	}{
+		"one coin": {
+			setupStore: func(ctx sdk.Context, s paramtypes.Subspace) {
+				s.SetParamSet(ctx, &types.Params{
+					MinimumGasPrices: sdk.NewDecCoins(sdk.NewDecCoin("ALX", sdk.OneInt())),
+				})
+			},
+			expMin: sdk.NewDecCoins(sdk.NewDecCoin("ALX", sdk.OneInt())),
+		},
+		"multiple coins": {
+			setupStore: func(ctx sdk.Context, s paramtypes.Subspace) {
+				s.SetParamSet(ctx, &types.Params{
+					MinimumGasPrices: sdk.NewDecCoins(sdk.NewDecCoin("ALX", sdk.OneInt()), sdk.NewDecCoin("BLX", sdk.NewInt(2))),
+				})
+			},
+			expMin: sdk.NewDecCoins(sdk.NewDecCoin("ALX", sdk.OneInt()), sdk.NewDecCoin("BLX", sdk.NewInt(2))),
+		},
+		"no min gas price set": {
+			setupStore: func(ctx sdk.Context, s paramtypes.Subspace) {
+				s.SetParamSet(ctx, &types.Params{})
+			},
+		},
+		"no param set": {
+			setupStore: func(ctx sdk.Context, s paramtypes.Subspace) {
+			},
+		},
 	}
-	s.setupGrpcQuerier(&globalFeeParams)
-	response, err := s.queryClient.MinimumGasPrices(s.ctx, &types.QueryMinimumGasPricesRequest{})
-	s.Require().NoError(err)
-	s.Require().Equal(response.MinimumGasPrices.String(), sdk.DecCoins{}.String())
-
-	// MinimumGasPrices: zero coin
-	zeroStake := sdk.DecCoins{sdk.NewDecCoin("stake", sdk.ZeroInt())}
-	globalFeeParams = types.Params{
-		MinimumGasPrices: zeroStake,
+	for name, spec := range specs {
+		t.Run(name, func(t *testing.T) {
+			ctx, _, subspace := setupTestStore(t)
+			spec.setupStore(ctx, subspace)
+			q := NewGrpcQuerier(subspace)
+			gotResp, gotErr := q.MinimumGasPrices(sdk.WrapSDKContext(ctx), nil)
+			require.NoError(t, gotErr)
+			require.NotNil(t, gotResp)
+			assert.Equal(t, spec.expMin, gotResp.MinimumGasPrices)
+		})
 	}
-	s.setupGrpcQuerier(&globalFeeParams)
-	response, err = s.queryClient.MinimumGasPrices(s.ctx, &types.QueryMinimumGasPricesRequest{})
-	s.Require().NoError(err)
-	s.Require().Equal(response.MinimumGasPrices.String(), zeroStake.String())
-
-	// MinimumGasPrices: mix coins
-	mixCoins := sdk.DecCoins{
-		sdk.NewDecCoin("photon", sdk.OneInt()),
-		sdk.NewDecCoin("stake", sdk.ZeroInt()),
-	}
-	globalFeeParams = types.Params{
-		MinimumGasPrices: mixCoins,
-	}
-	s.setupGrpcQuerier(&globalFeeParams)
-	response, err = s.queryClient.MinimumGasPrices(s.ctx, &types.QueryMinimumGasPricesRequest{})
-	s.Require().NoError(err)
-	s.Require().Equal(response.MinimumGasPrices.String(), mixCoins.String())
-}
-
-func (s *testSuite) setup() {
-	app := gaiahelpers.Setup(s.T(), false, 1)
-	ctx := app.BaseApp.NewContext(false, tmproto.Header{
-		ChainID: fmt.Sprintf("test-chain-%s", tmrand.Str(4)),
-		Height:  1,
-	})
-
-	encodingConfig := gaia.MakeTestEncodingConfig()
-	encodingConfig.Amino.RegisterConcrete(&testdata.TestMsg{}, "testdata.TestMsg", nil)
-	testdata.RegisterInterfaces(encodingConfig.InterfaceRegistry)
-
-	s.app = app
-	s.ctx = ctx
-}
-
-func (s *testSuite) setupGrpcQuerier(globalFeeParams *types.Params) {
-	subspace := s.app.GetSubspace(globalfee.ModuleName)
-	subspace.SetParamSet(s.ctx, globalFeeParams)
-
-	s.queryClient = globalfee.NewGrpcQuerier(subspace)
 }

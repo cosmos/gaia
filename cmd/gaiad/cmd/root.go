ab412be302da00562d7b13e7bc77d23d1667cd3d
Jacob Gadikian <jacobgadikian@gmail.com>
2023-01-10 13:05:59 +0100

    add cache size

--- cmd/gaiad/cmd/root.go
+++ cmd/gaiad/cmd/root.go
@@ -232,6 +232,8 @@ func (ac appCreator) newApp(
 		baseapp.SetSnapshotStore(snapshotStore),
 		baseapp.SetSnapshotInterval(cast.ToUint64(appOpts.Get(server.FlagStateSyncSnapshotInterval))),
 		baseapp.SetSnapshotKeepRecent(cast.ToUint32(appOpts.Get(server.FlagStateSyncSnapshotKeepRecent))),
+		baseapp.SetIAVLCacheSize(cast.ToInt(appOpts.Get(server.FlagIAVLCacheSize))),
+
 		baseapp.SetIAVLDisableFastNode(cast.ToBool(appOpts.Get(server.FlagDisableIAVLFastNode))),
 
 	)

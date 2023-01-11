96ff2a3b081654652b93951e060ee4b86ce8274f
Christian Borst <christianoborst@gmail.com>
2023-01-11 15:55:05 -0500

    TEMP: Add ante logging

--- x/lockup/ante.go
+++ x/lockup/ante.go
@@ -56,6 +56,7 @@ func (lad LockAnteDecorator) AnteHandle(
 	simulate bool,
 	next sdk.AnteHandler,
 ) (newCtx sdk.Context, err error) {
+	ctx.Logger().Info("Enter AnteHandle", "tx", tx)
 	if lad.lockupKeeper.GetChainLocked(ctx) {
 		lockedMsgTypesSet := lad.lockupKeeper.GetLockedMessageTypesSet(ctx)
 		exemptSet := lad.lockupKeeper.GetLockExemptAddressesSet(ctx)

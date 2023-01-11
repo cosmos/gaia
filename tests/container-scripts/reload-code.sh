3584b955f635c9ab86608baa1dffc3922897ea9e
Christian Borst <christianoborst@gmail.com>
2023-01-11 12:21:14 -0500

    Remove gravity-specific test script contents

--- tests/container-scripts/reload-code.sh
+++ tests/container-scripts/reload-code.sh
@@ -6,8 +6,7 @@ TEST_TYPE=$2
 set -eux
 
 # Stop any currently running peggy and eth processes
-pkill peggyd || true # allowed to fail
-pkill geth || true # allowed to fail
+pkill althea || true # allowed to fail
 
 # Wipe filesystem changes
 for i in $(seq 1 $NODES);
@@ -21,9 +20,6 @@ export PATH=$PATH:/usr/local/go/bin
 make install
 tests/container-scripts/setup-validators.sh $NODES
 tests/container-scripts/run-testnet.sh $NODES
-
-# deploy the ethereum contracts
-DEPLOY_CONTRACTS=1 RUST_BACKTRACE=full TEST_TYPE=$TEST_TYPE RUST_LOG=INFO test-runner
 
 # This keeps the script open to prevent Docker from stopping the container
 # immediately if the nodes are killed by a different process

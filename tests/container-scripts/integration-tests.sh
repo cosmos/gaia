1af1daff19139b8692ec2b444fdb0c8e7b5e8157
Christian Borst <christianoborst@gmail.com>
2023-01-11 13:26:37 -0500

    Remove check for contracts folder

--- tests/container-scripts/integration-tests.sh
+++ tests/container-scripts/integration-tests.sh
@@ -3,14 +3,6 @@ NODES=$1
 TEST_TYPE=$2
 set -eu
 
-FILE=/contracts
-if test -f "$FILE"; then
-echo "Contracts already deployed, running tests"
-else 
-echo "Testnet is not started yet, please wait before running tests"
-exit 0
-fi 
-
 set +e
 killall -9 test-runner
 set -e

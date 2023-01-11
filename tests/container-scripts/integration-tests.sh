31b634c176ee3443e95be9a1e322492728064e75
Christian Borst <christianoborst@gmail.com>
2023-01-11 15:44:57 -0500

    WIP

--- tests/container-scripts/integration-tests.sh
+++ tests/container-scripts/integration-tests.sh
@@ -7,4 +7,5 @@ set +e
 killall -9 test-runner
 set -e
 
-RUST_BACKTRACE=full CHAIN_BINARY=althea ADDRESS_PREFIX=althea STAKING_TOKEN=ualtg TEST_TYPE=$TEST_TYPE RUST_LOG=INFO test-runner
+pushd /althea/integration_tests/test_runner
+RUST_BACKTRACE=full TEST_TYPE=$TEST_TYPE RUST_LOG=INFO PATH=$PATH:$HOME/.cargo/bin cargo run --release --bin test-runner
--- tests/container-scripts/run-testnet.sh
+++ tests/container-scripts/run-testnet.sh
@@ -30,7 +30,7 @@ GRPC_ADDRESS="--grpc.address 7.7.7.$i:9091"
 fi
 LISTEN_ADDRESS="--address tcp://7.7.7.$i:26655"
 P2P_ADDRESS="--p2p.laddr tcp://7.7.7.$i:26656"
-LOG_LEVEL="--log_level error"
+LOG_LEVEL="--log_level info"
 ARGS="$GAIA_HOME $LISTEN_ADDRESS $RPC_ADDRESS $GRPC_ADDRESS $LOG_LEVEL $P2P_ADDRESS"
-$BIN $ARGS start > /validator$i/logs &
+$BIN $ARGS start &> /validator$i/logs &
 done

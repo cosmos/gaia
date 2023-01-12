265ef61ed501a492ca57c9d978e616fd9610ef23
Christian Borst <christianoborst@gmail.com>
2023-01-12 12:35:19 -0500

    Expose delve debugging port on test container

--- tests/start-chains.sh
+++ tests/start-chains.sh
@@ -15,4 +15,4 @@ NODES=3
 pushd $DIR/../
 
 # Run new test container instance
-docker run --name althea_test_instance --mount type=bind,source="$(pwd)"/,target=/althea --cap-add=NET_ADMIN -p 9090:9090 -p 26657:26657 -p 1317:1317 -p 8545:8545 -it althea-base /bin/bash /althea/tests/container-scripts/reload-code.sh $NODES
+docker run --name althea_test_instance --mount type=bind,source="$(pwd)"/,target=/althea --cap-add=NET_ADMIN -p 2345:2345 -p 9090:9090 -p 26657:26657 -p 1317:1317 -p 8545:8545 -it althea-base /bin/bash /althea/tests/container-scripts/reload-code.sh $NODES

b5eec77ad59071e3fdc821cb03275509bd4df0d3
Christian Borst <christianoborst@gmail.com>
2023-01-19 15:50:40 -0500

    Clear query example endpoint

--- x/microtx/keeper/grpc_query.go
+++ x/microtx/keeper/grpc_query.go
@@ -1,8 +1,6 @@
 package keeper
 
 import (
-	"context"
-
 	"github.com/althea-net/althea-chain/x/microtx/types"
 	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
 	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
@@ -13,10 +11,4 @@ var _ types.QueryServer = Keeper{
 	paramSpace: paramstypes.Subspace{},
 	cdc:        nil,
 	bankKeeper: &bankkeeper.BaseKeeper{},
-}
-
-// Data is an example query endpoint
-func (k Keeper) Data(c context.Context, req *types.QueryData) (*types.QueryDataResponse, error) {
-	// TODO: fetch the data from the keeper, format, return
-	return nil, nil
 }

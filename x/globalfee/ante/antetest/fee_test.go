b182e5fc82e7e04ca12ff9ada802a6f0c99aa10e
lg <8335464+glnro@users.noreply.github.com>
2023-01-23 10:24:31 +0100

    chore: bump ibc ics and pfm for ibc v4 (#2054)
    * Bump ibc ics and pfm for ibc v4
    
    * Fix ics version

--- x/globalfee/ante/antetest/fee_test.go
+++ x/globalfee/ante/antetest/fee_test.go
@@ -7,8 +7,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
-	ibcclienttypes "github.com/cosmos/ibc-go/v3/modules/core/02-client/types"
-	ibcchanneltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
+	ibcclienttypes "github.com/cosmos/ibc-go/v4/modules/core/02-client/types"
+	ibcchanneltypes "github.com/cosmos/ibc-go/v4/modules/core/04-channel/types"
 	"github.com/stretchr/testify/suite"
 
 	gaiaapp "github.com/cosmos/gaia/v9/app"

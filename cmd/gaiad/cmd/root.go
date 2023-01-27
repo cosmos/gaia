e1716b8b5bd4f7f5cfd46909df52f9a081dcfad0
Matija Salopek <matija.salopek994@gmail.com>
2023-01-27 19:08:20 +0100

    change bypass-min-fee-types parsing; change defaults

--- cmd/gaiad/cmd/root.go
+++ cmd/gaiad/cmd/root.go
@@ -83,7 +83,7 @@ func initAppConfig() (string, interface{}) {
 
 	return params.CustomConfigTemplate(), params.CustomAppConfig{
 		Config:               *srvCfg,
-		BypassMinFeeMsgTypes: gaia.GetDefaultBypassFeeMessages(),
+		BypassMinFeeMsgTypes: []string{},
 	}
 }
 

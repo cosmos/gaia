083bdb55752516800c4124834005ba2d796e9b55
MSalopek <35486649+MSalopek@users.noreply.github.com>
2023-03-17 15:51:56 +0100

    change bypass-min-fee-types parsing; change defaults (#2092)
    * change bypass-min-fee-types parsing; change defaults
    
    * revert default message registration
    
    * docs: add changelog for fixing bypass parsing
    
    * Update CHANGELOG.md
    
    Co-authored-by: Simon Noetzlin <simon.ntz@gmail.com>
    
    ---------
    
    Co-authored-by: Yaru Wang <yaru@informal.systems>
    Co-authored-by: yaruwangway <69694322+yaruwangway@users.noreply.github.com>
    Co-authored-by: Simon Noetzlin <simon.ntz@gmail.com>
    (cherry picked from commit 4badbcdfe54eb68a2b7ad4018919b2e261c446b9)
    
    # Conflicts:
    #	CHANGELOG.md

--- app/params/config.go
+++ app/params/config.go
@@ -19,9 +19,14 @@ var (
 ###############################################################################
 # bypass-min-fee-msg-types defines custom message types the operator may set that
 # will bypass minimum fee checks during CheckTx.
+# NOTE:
+# bypass-min-fee-msg-types = [] will deactivate the bypass - no messages will be allowed to bypass the minimum fee check
+# bypass-min-fee-msg-types = [<MsgType>...] will allow messages of specified type to bypass the minimum fee check
+# removing bypass-min-fee-msg-types from the config file will apply the default values:
+# ["/ibc.core.channel.v1.MsgRecvPacket", "/ibc.core.channel.v1.MsgAcknowledgement", "/ibc.core.client.v1.MsgUpdateClient"]
 #
 # Example:
-# ["/ibc.core.channel.v1.MsgRecvPacket", "/ibc.core.channel.v1.MsgAcknowledgement", ...]
+# bypass-min-fee-msg-types = ["/ibc.core.channel.v1.MsgRecvPacket", "/ibc.core.channel.v1.MsgAcknowledgement", "/ibc.core.client.v1.MsgUpdateClient"]
 bypass-min-fee-msg-types = [{{ range .BypassMinFeeMsgTypes }}{{ printf "%q, " . }}{{end}}]
 `
 )
@@ -42,5 +47,9 @@ type CustomAppConfig struct {
 
 	// BypassMinFeeMsgTypes defines custom message types the operator may set that
 	// will bypass minimum fee checks during CheckTx.
+	// NOTE:
+	// bypass-min-fee-msg-types = [] will deactivate the bypass - no messages will be allowed to bypass the minimum fee check
+	// bypass-min-fee-msg-types = [<some_msg_type>] will allow messages of specified type to bypass the minimum fee check
+	// omitting bypass-min-fee-msg-types from the config file will use the default values: ["/ibc.core.channel.v1.MsgRecvPacket", "/ibc.core.channel.v1.MsgAcknowledgement", "/ibc.core.client.v1.MsgUpdateClient"]
 	BypassMinFeeMsgTypes []string `mapstructure:"bypass-min-fee-msg-types"`
 }

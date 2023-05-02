664ed2661336338ed3d719c2446715fc24aebc1f
Simon Noetzlin <simon.ntz@gmail.com>
2023-05-02 10:42:47 +0200

    clean up script

--- contrib/single-node.sh
+++ contrib/single-node.sh
@@ -15,8 +15,6 @@ if [ -z "$2" ]; then
   exit 1
 fi
 
-rm -rf /Users/simon/.gaia
-
 # Build genesis file incl account for passed address
 coins="10000000000stake,100000000000samoleans"
 gaiad init --chain-id $CHAINID $CHAINID
@@ -27,12 +25,10 @@ gaiad gentx validator 5000000000stake --keyring-backend="test" --chain-id $CHAIN
 gaiad collect-gentxs
 
 # Set proper defaults and change ports
-echo 1
-sed -i '' 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' ~/.gaia/config/config.toml
-echo 2
-sed -i '' 's/timeout_commit = "5s"/timeout_commit = "1s"/g' ~/.gaia/config/config.toml
-sed -i '' 's/timeout_propose = "3s"/timeout_propose = "1s"/g' ~/.gaia/config/config.toml
-sed -i '' 's/index_all_keys = false/index_all_keys = true/g' ~/.gaia/config/config.toml
+sed -i 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' ~/.gaia/config/config.toml
+sed -i 's/timeout_commit = "5s"/timeout_commit = "1s"/g' ~/.gaia/config/config.toml
+sed -i 's/timeout_propose = "3s"/timeout_propose = "1s"/g' ~/.gaia/config/config.toml
+sed -i 's/index_all_keys = false/index_all_keys = true/g' ~/.gaia/config/config.toml
 
 # Start the gaia
 gaiad start --pruning=nothing

00b3335ab69c41f233bb6f2c7fb50dd41844396a
Simon Noetzlin <simon.ntz@gmail.com>
2023-05-08 10:14:16 +0200

    fix flag fees in e2e tests

--- tests/e2e/e2e_vesting_test.go
+++ tests/e2e/e2e_vesting_test.go
@@ -38,7 +38,7 @@ var (
 	vestingAmount           = sdk.NewCoin(uatomDenom, sdk.NewInt(350000))
 	vestingBalance          = sdk.NewCoins(vestingAmountVested).Add(vestingAmount)
 	vestingDelegationAmount = sdk.NewCoin(uatomDenom, sdk.NewInt(500000000))
-	vestingDelegationFees   = sdk.NewCoin(uatomDenom, sdk.NewInt(10))
+	vestingDelegationFees   = sdk.NewCoin(uatomDenom, sdk.NewInt(1))
 )
 
 func (s *IntegrationTestSuite) testDelayedVestingAccount(api string) {

4b6a16c5266575faf63b98d5d4fba4161367f133
Simon Noetzlin <simon.ntz@gmail.com>
2023-06-05 18:21:33 +0200

    Update app/params/proto.go

--- app/params/proto.go
+++ app/params/proto.go
@@ -6,8 +6,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
 )
 
-// EncodingConfig specifies the concrete encoding types to use for a given app.
-// This is provided for compatibility between protobuf and amino implementations.
+// MakeEncodingConfig creates an EncodingConfig for an amino based test configuration.
 func MakeEncodingConfig() EncodingConfig {
 	amino := codec.NewLegacyAmino()
 	interfaceRegistry := codectypes.NewInterfaceRegistry()

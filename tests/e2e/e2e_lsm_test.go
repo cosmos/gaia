66e29ad6fb630e6092617ede7c6c36e33c351696
jstr1121 <jun@stridelabs.co>
2023-07-14 20:22:04 +0800

    add tokenize share check on lsm test & redeemShare utility function

--- tests/e2e/e2e_lsm_test.go
+++ tests/e2e/e2e_lsm_test.go
@@ -2,6 +2,8 @@ package e2e
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -23,6 +25,22 @@ func (s *IntegrationTestSuite) testLSM() {
 
 	fees := sdk.NewCoin(uatomDenom, sdk.NewInt(1))
 
+	// Validator bond
+	s.executeValidatorBond(s.chainA, 0, validatorAddressA, validatorAAddr.String(), gaiaHomePath, fees.String())
+
+	// Validate validator bond successful
+	s.Require().Eventually(
+		func() bool {
+			res, err := queryDelegation(chainEndpoint, validatorAddressA, validatorAAddr.String())
+			isValidatorBond := res.GetDelegationResponse().GetDelegation().ValidatorBond
+			s.Require().NoError(err)
+
+			return isValidatorBond == true
+		},
+		20*time.Second,
+		5*time.Second,
+	)
+
 	delegationAmount := sdk.NewInt(500000000)
 	delegation := sdk.NewCoin(uatomDenom, delegationAmount) // 500 atom
 
@@ -42,24 +60,37 @@ func (s *IntegrationTestSuite) testLSM() {
 		5*time.Second,
 	)
 
-	// Validator bond
-	s.executeValidatorBond(s.chainA, 0, validatorAddressA, validatorAAddr.String(), gaiaHomePath, fees.String())
+	// Tokenize shares
+	tokenizeAmount := sdk.NewInt(200000000)
+	tokenize := sdk.NewCoin(uatomDenom, tokenizeAmount) // 200 atom
+	s.executeTokenizeShares(s.chainA, 0, tokenize.String(), validatorAddressA, delegatorAddress, gaiaHomePath, fees.String())
 
-	// Validate validator bond successful
+	// Validate delegation reduced
 	s.Require().Eventually(
 		func() bool {
-			res, err := queryDelegation(chainEndpoint, validatorAddressA, validatorAAddr.String())
-			isValidatorBond := res.GetDelegationResponse().GetDelegation().ValidatorBond
+			res, err := queryDelegation(chainEndpoint, validatorAddressA, delegatorAddress)
+			amt := res.GetDelegationResponse().GetDelegation().GetShares()
 			s.Require().NoError(err)
 
-			return isValidatorBond == true
+			return amt.Equal(sdk.NewDecFromInt(delegationAmount.Sub(tokenizeAmount)))
 		},
 		20*time.Second,
 		5*time.Second,
 	)
 
-	// TODO: Delegates
-	// TODO: Tokenizes shares
+	// Validate balance increased
+	shareDenom := fmt.Sprintf("%s/%s", strings.ToLower(validatorAddressA), strconv.Itoa(1))
+	s.Require().Eventually(
+		func() bool {
+			res, err := queryGaiaAllBalances(chainEndpoint, delegatorAddress)
+			s.Require().NoError(err)
+			shareAmount := res.AmountOf(shareDenom)
+			return shareAmount.Equal(tokenizeAmount)
+		},
+		20*time.Second,
+		5*time.Second,
+	)
+
 	// TODO: Bank send LSM token
 	// TODO: TransferTokenizeShareRecord (transfer reward ownership)
 	// TODO: IBC transfer LSM token

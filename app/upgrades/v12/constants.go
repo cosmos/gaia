d00bd5ecdf459c544b814897455b73971c655cdf
sampocs <sam.pochyly@gmail.com>
2023-08-04 09:18:04 -0500

    added comments around constants

--- app/upgrades/v12/constants.go
+++ app/upgrades/v12/constants.go
@@ -8,9 +8,16 @@ const (
 	// UpgradeName defines the on-chain upgrade name.
 	UpgradeName = "v12"
 
-	ValidatorBondFactor       = 250
-	ValidatorLiquidStakingCap = 50
-	GlobalLiquidStakingCap    = 25
+	// The ValidatorBondFactor dictates the cap on the liquid shares
+	// for a validator - determined as a multiple to their validator bond
+	// (e.g. ValidatorBondShares = 1000, BondFactor = 250 -> LiquidSharesCap: 250,000)
+	ValidatorBondFactor = 250
+	// GlobalLiquidStakingCap represents the percentage cap on
+	// the portion of a validator's stake that can be liquid
+	ValidatorLiquidStakingCap = 50 // 50%
+	// GlobalLiquidStakingCap represents the percentage cap on
+	// the portion of a chain's total stake can be liquid
+	GlobalLiquidStakingCap = 25 // 25%
 )
 
 var Upgrade = upgrades.Upgrade{

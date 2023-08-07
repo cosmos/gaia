003c089ec8a72f1f16bf0c0ed28b11edbecefdf3
sampocs <sam@stridelabs.co>
2023-08-07 11:07:04 -0500

    Add lsm params to upgrade handler (#2686)
    * set LSM params in upgrade handler
    
    * added comments around constants
    
    * test: add migrateUBDEntries to upgrade handler — required adding exported.go to sdk
    
    * Revert "test: add migrateUBDEntries to upgrade handler — required adding exported.go to sdk"
    
    This reverts commit 7bd9727ce734e829ef54538485c3bbf327009345.
    
    ---------
    
    Co-authored-by: Riley Edmunds <riley@stridelabs.co>

--- app/upgrades/v12/constants.go
+++ app/upgrades/v12/constants.go
@@ -7,6 +7,17 @@ import (
 const (
 	// UpgradeName defines the on-chain upgrade name.
 	UpgradeName = "v12"
+
+	// The ValidatorBondFactor dictates the cap on the liquid shares
+	// for a validator - determined as a multiple to their validator bond
+	// (e.g. ValidatorBondShares = 1000, BondFactor = 250 -> LiquidSharesCap: 250,000)
+	ValidatorBondFactor = 250
+	// GlobalLiquidStakingCap represents the percentage cap on
+	// the portion of a validator's stake that can be liquid
+	ValidatorLiquidStakingCap = 50 // 50%
+	// GlobalLiquidStakingCap represents the percentage cap on
+	// the portion of a chain's total stake can be liquid
+	GlobalLiquidStakingCap = 25 // 25%
 )
 
 var Upgrade = upgrades.Upgrade{

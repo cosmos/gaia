555360a140c144bcd013a23a2c2d068121b42578
Milan Mulji <98309852+mmulji-ic@users.noreply.github.com>
2023-09-15 20:26:57 +0200

    Mava integration trial

--- docs/src/js/Var.js
+++ docs/src/js/Var.js
@@ -3,3 +3,16 @@ import React from 'react';
 export const Var = ({ children }) => {
   return <code>{JSON.stringify(children)}</code>;
 };
+
+export const PlainVar = ({ children }) => {
+  return <span>{JSON.stringify(children)}</span>;
+};
+
+export const ConsoleOutput = ({ children }) => {
+  
+  return (
+    <textarea readonly className='text-left'>
+      {JSON.stringify(children.replace(/\n/g, "\r"))}
+    </textarea>
+  )
+};

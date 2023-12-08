cd15bc50c576cfca733a8a144d62301596597f5c
MSalopek <matija.salopek994@gmail.com>
2023-12-08 13:33:13 +0100

    scripts: update hermes bootstrap scripts

--- tests/e2e/scripts/hermes1_bootstrap.sh
+++ tests/e2e/scripts/hermes1_bootstrap.sh
@@ -47,7 +47,7 @@ port = 3002
 id = '$GAIA_A_E2E_CHAIN_ID'
 rpc_addr = 'http://$GAIA_A_E2E_VAL_HOST:26657'
 grpc_addr = 'http://$GAIA_A_E2E_VAL_HOST:9090'
-websocket_addr = 'ws://$GAIA_A_E2E_VAL_HOST:26657/websocket'
+event_source = { mode = "push", url = "ws://$GAIA_A_E2E_VAL_HOST:26657/websocket", batch_delay = "50ms" }
 rpc_timeout = '10s'
 account_prefix = 'cosmos'
 key_name = 'rly01-gaia-a'
@@ -63,7 +63,7 @@ trust_threshold = { numerator = '1', denominator = '3' }
 id = '$GAIA_B_E2E_CHAIN_ID'
 rpc_addr = 'http://$GAIA_B_E2E_VAL_HOST:26657'
 grpc_addr = 'http://$GAIA_B_E2E_VAL_HOST:9090'
-websocket_addr = 'ws://$GAIA_B_E2E_VAL_HOST:26657/websocket'
+event_source = { mode = "push", url = "ws://$GAIA_B_E2E_VAL_HOST:26657/websocket", batch_delay = "50ms" }
 rpc_timeout = '10s'
 account_prefix = 'cosmos'
 key_name = 'rly01-gaia-b'
@@ -114,7 +114,7 @@ port = 3002
 id = '$GAIA_A_E2E_CHAIN_ID'
 rpc_addr = 'http://$GAIA_A_E2E_VAL_HOST:26657'
 grpc_addr = 'http://$GAIA_A_E2E_VAL_HOST:9090'
-websocket_addr = 'ws://$GAIA_A_E2E_VAL_HOST:26657/websocket'
+event_source = { mode = "push", url = "ws://$GAIA_A_E2E_VAL_HOST:26657/websocket", batch_delay = "50ms" }
 rpc_timeout = '10s'
 account_prefix = 'cosmos'
 key_name = 'rly01-gaia-a'
@@ -130,7 +130,7 @@ trust_threshold = { numerator = '1', denominator = '3' }
 id = '$GAIA_B_E2E_CHAIN_ID'
 rpc_addr = 'http://$GAIA_B_E2E_VAL_HOST:26657'
 grpc_addr = 'http://$GAIA_B_E2E_VAL_HOST:9090'
-websocket_addr = 'ws://$GAIA_B_E2E_VAL_HOST:26657/websocket'
+event_source = { mode = "push", url = "ws://$GAIA_B_E2E_VAL_HOST:26657/websocket", batch_delay = "50ms" }
 rpc_timeout = '10s'
 account_prefix = 'cosmos'
 key_name = 'rly01-gaia-b'

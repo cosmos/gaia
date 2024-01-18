18db7934897e3688490b513c37af0a5364a7b4e3
Simon Noetzlin <simon.ntz@gmail.com>
2024-01-18 09:49:33 +0100

    nit

--- app/upgrades/v15/upgrades_test.go
+++ app/upgrades/v15/upgrades_test.go
@@ -16,7 +16,7 @@ import (
 	v15 "github.com/cosmos/gaia/v15/app/upgrades/v15"
 )
 
-func TestV15UpgradeHandler(t *testing.T) {
+func TestUpgradeCommissionRate(t *testing.T) {
 	gaiaApp := helpers.Setup(t)
 	ctx := gaiaApp.NewUncachedContext(true, tmproto.Header{})
 

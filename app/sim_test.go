52a0f44d126d1509d555792f82af8281f2b2fc0e
MSalopek <matija.salopek994@gmail.com>
2024-02-06 13:06:22 +0100

    fix!: check v1 and v1beta1 MsgVote in VoteSpamDecorator (#2923)
    * fix!: check v1 and v1beta1 MsgVote in VoteSpamDecorator
    
    https://github.com/cosmos/gaia/pull/2912 introuced the VoteSpamDecorator.
    
    The implementation was not complete and only v1beta1 gov types were handled.
    
    The CLI supports v1 gov types so the validation was not sufficient.
    
    This commit expands the antehandler validation to allow correct processing of both v1 and v1beta1 messages.
    
    * appease linter
    
    * test: stop app from failing non-determinism test on gov vote
    
    * ante: handle edgecase with 0 minStakedTokens
    
    * appease linter

--- app/sim_test.go
+++ app/sim_test.go
@@ -12,6 +12,8 @@ import (
 	dbm "github.com/cometbft/cometbft-db"
 	"github.com/cometbft/cometbft/libs/log"
 
+	"cosmossdk.io/math"
+
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/server"
@@ -21,6 +23,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/simulation"
 	simcli "github.com/cosmos/cosmos-sdk/x/simulation/client/cli"
 
+	"github.com/cosmos/gaia/v15/ante"
 	gaia "github.com/cosmos/gaia/v15/app"
 	// "github.com/cosmos/gaia/v11/app/helpers"
 	// "github.com/cosmos/gaia/v11/app/params"
@@ -96,6 +99,10 @@ func TestAppStateDeterminism(t *testing.T) {
 				interBlockCacheOpt(),
 				baseapp.SetChainID(AppChainID),
 			)
+
+			// NOTE: setting to zero to avoid failing the simulation
+			// due to the minimum staked tokens required to submit a vote
+			ante.SetMinStakedTokens(math.LegacyZeroDec())
 
 			fmt.Printf(
 				"running non-determinism simulation; seed %d: %d/%d, attempt: %d/%d\n",

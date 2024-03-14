09c4f746030e18a5dfa439ca5c5d928abfcf919a
dusan-maksimovic <dusan.maksimovic@ethernal.tech>
2024-03-14 15:45:17 +0100

    Added docs for completeChannelHandshakeFromTry()
    Co-authored-by: Stana Miric <stana.miric@ethernal.tech>

--- tests/e2e/e2e_ibc_test.go
+++ tests/e2e/e2e_ibc_test.go
@@ -208,6 +208,9 @@ func (s *IntegrationTestSuite) createChannel() {
 	s.T().Logf("IBC transfer channel created between chains %s and %s", s.chainA.id, s.chainB.id)
 }
 
+// This function will complete the channel handshake in cases when ChanOpenInit was initiated
+// by some transaction that was previously executed on the chain. For example,
+// ICA MsgRegisterInterchainAccount will perform ChanOpenInit during its execution.
 func (s *IntegrationTestSuite) completeChannelHandshakeFromTry(
 	srcChain, dstChain,
 	srcConnection, dstConnection,

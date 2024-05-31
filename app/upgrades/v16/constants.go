openlab-openos/cosmos

Removed globalfee from app
Co-authored-by: Dusan Maksimovic <dusan.maksimovic@ethernal.tech>

app/upgrades/v16/constants.go
@@ -3,6 +3,7 @@ package v16
 import (
 	store "github.com/cosmos/cosmos-sdk/store/types"
 	"github.com/cosmos/gaia/v16/app/upgrades"
+	"github.com/cosmos/gaia/v16/x/globalfee"
 	feemarkettypes "github.com/skip-mev/feemarket/x/feemarket/types"
 )
 
@@ -17,6 +18,9 @@ var Upgrade = upgrades.Upgrade{
 	StoreUpgrades: store.StoreUpgrades{
 		Added: []string{
 			feemarkettypes.ModuleName,
+		},
+		Deleted: []string{
+			globalfee.ModuleName,
 		},
 	},
 }

cosmos/gaia

app/upgrades/v16/constants.go
@@ -9,6 +9,7 @@ import (
 	store "github.com/cosmos/cosmos-sdk/store/types"
 
 	"github.com/cosmos/gaia/v18/app/upgrades"
+	"github.com/cosmos/gaia/v18/x/globalfee"
 	feemarkettypes "github.com/skip-mev/feemarket/x/feemarket/types"
 )
 
@@ -29,6 +30,9 @@ var Upgrade = upgrades.Upgrade{
 			icacontrollertypes.SubModuleName,
 			ibcfeetypes.ModuleName,
 			feemarkettypes.ModuleName,
+		},
+		Deleted: []string{
+			globalfee.ModuleName,
 		},
 	},
 }
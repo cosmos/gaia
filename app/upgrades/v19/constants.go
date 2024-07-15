068aadff31ea33a12974ce6497964ef6dbadffe6
MSalopek <matija.salopek994@gmail.com>
2024-07-15 14:15:16 +0200

    chore: add v19 folder

---
+++ app/upgrades/v19/constants.go
@@ -0,0 +1,15 @@
+package v19
+
+import (
+	"github.com/cosmos/gaia/v19/app/upgrades"
+)
+
+const (
+	// UpgradeName defines the on-chain upgrade name.
+	UpgradeName = "v19"
+)
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+}

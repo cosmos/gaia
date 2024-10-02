0669614769de53a6a2af8028dde87111aaef2c36
MSalopek <matija.salopek994@gmail.com>
2024-10-02 15:17:44 +0200

    appease linters

--- app/sim_test.go
+++ app/sim_test.go
@@ -25,7 +25,6 @@ import (
 
 	"github.com/cosmos/gaia/v21/ante"
 	gaia "github.com/cosmos/gaia/v21/app"
-
 	// "github.com/cosmos/gaia/v11/app/helpers"
 	// "github.com/cosmos/gaia/v11/app/params"
 	"github.com/cosmos/gaia/v21/app/sim"

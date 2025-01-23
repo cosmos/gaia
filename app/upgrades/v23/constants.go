ce2ec2c92c1140fdb2dba4ecab5889e8f2364c92
Eric Warehime <eric.warehime@gmail.com>
2025-01-22 16:23:33 -0800

    Fix upgrade

--- app/upgrades/v23/constants.go
+++ app/upgrades/v23/constants.go
@@ -1,7 +1,10 @@
 package v23
 
 import (
+	storetypes "cosmossdk.io/store/types"
+
 	"github.com/cosmos/gaia/v23/app/upgrades"
+	lsmtypes "github.com/cosmos/gaia/v23/x/lsm/types"
 )
 
 const (
@@ -12,4 +15,9 @@ const (
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: storetypes.StoreUpgrades{
+		Added: []string{
+			lsmtypes.ModuleName,
+		},
+	},
 }

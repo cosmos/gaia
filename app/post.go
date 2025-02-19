a53adad566e67349fc5a5cf77c55a728d1dcacc7
Vlad <vladjdk@gmail.com>
2025-02-19 16:23:24 -0500

    split post up for two transaction types (todo see if this is necessary)

--- app/post.go
+++ app/post.go
@@ -1,6 +1,7 @@
 package gaia
 
 import (
+	"github.com/cosmos/cosmos-sdk/x/auth/ante"
 	"github.com/cosmos/gaia/v23/ante/cosmos"
 	feemarketpost "github.com/skip-mev/feemarket/x/feemarket/post"
 
@@ -19,29 +20,49 @@ type PostHandlerOptions struct {
 
 // NewPostHandler returns a PostHandler chain with the fee deduct decorator.
 func NewPostHandler(options PostHandlerOptions) (sdk.PostHandler, error) {
-	if !cosmos.UseFeeMarketDecorator {
-		return nil, nil
-	}
+	return func(ctx sdk.Context, tx sdk.Tx, simulate bool, success bool) (newCtx sdk.Context, err error) {
+		var postHandler sdk.PostHandler
+		txWithExtensions, ok := tx.(ante.HasExtensionOptionsTx)
+		if ok {
+			opts := txWithExtensions.GetExtensionOptions()
+			if len(opts) > 0 {
+				switch typeUrl := opts[0].GetTypeUrl(); typeUrl {
+				case "/ethermint.evm.v1.ExtensionOptionsEthereumTx":
+					postHandler = sdk.ChainPostDecorators()
+				default:
+					return ctx, errorsmod.Wrapf(
+						sdkerrors.ErrUnknownExtensionOptions,
+						"rejecting tx with unsupported extension type: %s", typeUrl,
+					)
+				}
+				return postHandler(ctx, tx, simulate, success)
+			}
+		}
+		if !cosmos.UseFeeMarketDecorator {
+			return ctx, nil
+		}
 
-	if options.AccountKeeper == nil {
-		return nil, errorsmod.Wrap(sdkerrors.ErrLogic, "account keeper is required for post builder")
-	}
+		if options.AccountKeeper == nil {
+			return ctx, errorsmod.Wrap(sdkerrors.ErrLogic, "account keeper is required for post builder")
+		}
 
-	if options.BankKeeper == nil {
-		return nil, errorsmod.Wrap(sdkerrors.ErrLogic, "bank keeper is required for post builder")
-	}
+		if options.BankKeeper == nil {
+			return ctx, errorsmod.Wrap(sdkerrors.ErrLogic, "bank keeper is required for post builder")
+		}
 
-	if options.FeeMarketKeeper == nil {
-		return nil, errorsmod.Wrap(sdkerrors.ErrLogic, "feemarket keeper is required for post builder")
-	}
+		if options.FeeMarketKeeper == nil {
+			return ctx, errorsmod.Wrap(sdkerrors.ErrLogic, "feemarket keeper is required for post builder")
+		}
 
-	postDecorators := []sdk.PostDecorator{
-		feemarketpost.NewFeeMarketDeductDecorator(
-			options.AccountKeeper,
-			options.BankKeeper,
-			options.FeeMarketKeeper,
-		),
-	}
+		postDecorators := []sdk.PostDecorator{
+			feemarketpost.NewFeeMarketDeductDecorator(
+				options.AccountKeeper,
+				options.BankKeeper,
+				options.FeeMarketKeeper,
+			),
+		}
 
-	return sdk.ChainPostDecorators(postDecorators...), nil
+		return sdk.ChainPostDecorators(postDecorators...)(ctx, tx, simulate, success)
+	}, nil
+
 }

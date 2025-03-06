b468ea8a8a5cee727df42a6521fe1fc4a476a37f
Vlad <vladjdk@gmail.com>
2025-03-06 16:14:56 -0500

    skip go test ibc classic

--- tests/e2e/query.go
+++ tests/e2e/query.go
@@ -3,14 +3,13 @@ package e2e
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"strings"
-
 	ratelimittypes "github.com/cosmos/ibc-apps/modules/rate-limiting/v10/types"
 	wasmclienttypes "github.com/cosmos/ibc-go/modules/light-clients/08-wasm/v10/types"
 	icacontrollertypes "github.com/cosmos/ibc-go/v10/modules/apps/27-interchain-accounts/controller/types"
 	providertypes "github.com/cosmos/interchain-security/v7/x/ccv/provider/types"
+	"io"
+	"net/http"
+	"strings"
 
 	evidencetypes "cosmossdk.io/x/evidence/types"
 

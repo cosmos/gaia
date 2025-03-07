017d5b0a43cd7bc2beeabed6a686a9d712a19575
Eric Warehime <eric.warehime@gmail.com>
2025-03-07 11:21:21 -0800

    Fix upgrades

--- app/upgrades/v23/constants.go
+++ app/upgrades/v23/constants.go
@@ -1,8 +1,6 @@
 package v23
 
 import (
-	_ "embed"
-
 	ibcwasmtypes "github.com/cosmos/ibc-go/modules/light-clients/08-wasm/v10/types"
 
 	"cosmossdk.io/store/types"
@@ -18,12 +16,7 @@ const (
 	IbcFeeStoreKey          = "feeibc"
 	ClientUploaderAddress   = "cosmos1raa4kyx5ypz75qqk3566c6slx2mw3qzs5ps5du"
 	IBCWasmStoreCodeTypeURL = "/ibc.lightclients.wasm.v1.MsgStoreCode"
-
-	ExpectedEthLightClientChecksum = "f82549f5bc8adaef18e5ce4f5b68269947343742c938dac322faf1583319172c"
 )
-
-//go:embed cw_ics08_wasm_eth.wasm.gz
-var ethWasmLightClient []byte
 
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,

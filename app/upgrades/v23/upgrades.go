f38a3a90cefedd606c20852211fb51c6409afafe
Eric Warehime <eric.warehime@gmail.com>
2025-03-07 15:13:49 -0500

    fix!: Add upgrade handler (#3610)
    * Add upgrade
    
    * Format
    
    * Add address
    
    * Fix upgrade
    
    * Remove eth light client
    
    * Fix comment
    
    * Fix upgrades

--- app/upgrades/v23/upgrades.go
+++ app/upgrades/v23/upgrades.go
@@ -2,10 +2,7 @@ package v23
 
 import (
 	"context"
-	"encoding/hex"
-	"fmt"
 
-	ibcwasmkeeper "github.com/cosmos/ibc-go/modules/light-clients/08-wasm/v10/keeper"
 	ibcwasmtypes "github.com/cosmos/ibc-go/modules/light-clients/08-wasm/v10/types"
 	ibctmtypes "github.com/cosmos/ibc-go/v10/modules/light-clients/07-tendermint"
 
@@ -14,9 +11,33 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/authz"
+	authzkeeper "github.com/cosmos/cosmos-sdk/x/authz/keeper"
+	govkeeper "github.com/cosmos/cosmos-sdk/x/gov/keeper"
 
 	"github.com/cosmos/gaia/v23/app/keepers"
 )
+
+// CreateRCUpgradeHandler returns an upgrade handler for Gaia v23.0.0-rc3.
+// This should only be executed on networks which have already run a previous v23 upgrade handler.
+func CreateRCUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	keepers *keepers.AppKeepers,
+) upgradetypes.UpgradeHandler {
+	return func(c context.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		ctx := sdk.UnwrapSDKContext(c)
+		ctx.Logger().Info("Starting custom migration...")
+
+		if err := AuthzGrantWasmLightClient(c, keepers.AuthzKeeper, *keepers.GovKeeper); err != nil {
+			ctx.Logger().Error("Error running authz grant for ibc wasm client", "message", err.Error())
+			return vm, err
+		}
+
+		ctx.Logger().Info("Upgrade v23.0.0-rc3 complete")
+		return vm, nil
+	}
+}
 
 // CreateUpgradeHandler returns an upgrade handler for Gaia v23.
 func CreateUpgradeHandler(
@@ -39,10 +60,9 @@ func CreateUpgradeHandler(
 		params.AllowedClients = []string{ibctmtypes.ModuleName, ibcwasmtypes.ModuleName}
 		keepers.IBCKeeper.ClientKeeper.SetParams(ctx, params)
 
-		// Add Eth Light Wasm Light Client
-		ctx.Logger().Info("Adding Eth Light Wasm Light Client")
-		if err := AddEthLightWasmLightClient(ctx, keepers.WasmClientKeeper); err != nil {
-			ctx.Logger().Error("Error adding Eth Light Wasm Light Client", "message", err.Error())
+		ctx.Logger().Info("Running authz ibc wasm client grant")
+		if err := AuthzGrantWasmLightClient(ctx, keepers.AuthzKeeper, *keepers.GovKeeper); err != nil {
+			ctx.Logger().Error("Error running authz grant for ibc wasm client", "message", err.Error())
 			return nil, err
 		}
 
@@ -51,20 +71,27 @@ func CreateUpgradeHandler(
 	}
 }
 
-func AddEthLightWasmLightClient(ctx context.Context, wasmKeeper ibcwasmkeeper.Keeper) error {
-	resp, err := wasmKeeper.StoreCode(ctx, &ibcwasmtypes.MsgStoreCode{
-		Signer:       wasmKeeper.GetAuthority(),
-		WasmByteCode: ethWasmLightClient,
+func AuthzGrantWasmLightClient(ctx context.Context, authzKeeper authzkeeper.Keeper, govKeeper govkeeper.Keeper) error {
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	grant, err := authz.NewGrant(
+		sdkCtx.BlockTime(),
+		authz.NewGenericAuthorization(IBCWasmStoreCodeTypeURL),
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+	sdkCtx.Logger().Info("Granting IBC Wasm Store Code", "granter", govKeeper.GetAuthority(), "grantee", ClientUploaderAddress)
+	resp, err := authzKeeper.Grant(ctx, &authz.MsgGrant{
+		Granter: govKeeper.GetAuthority(),
+		Grantee: ClientUploaderAddress,
+		Grant:   grant,
 	})
 	if err != nil {
-		return errorsmod.Wrap(err, "failed to store eth wasm light client during upgrade")
+		return err
 	}
-
-	actualChecksum := hex.EncodeToString(resp.Checksum)
-
-	if hex.EncodeToString(resp.Checksum) != ExpectedEthLightClientChecksum {
-		return fmt.Errorf("checksum mismatch: expected %s, got %s", ExpectedEthLightClientChecksum, actualChecksum)
+	if resp != nil {
+		sdkCtx.Logger().Info("Authz Keeper Grant", "response", resp.String())
 	}
-
 	return nil
 }

5a7f052ec9218def1cd5c5707f0cc3bda3e768fb
Eric Warehime <eric.warehime@gmail.com>
2025-03-27 15:19:41 -0700

    feat: Uniformly treat liquid tokens (#3668)

--- tests/e2e/e2e_liquid_test.go
+++ tests/e2e/e2e_liquid_test.go
@@ -14,6 +14,9 @@ import (
 	liquidtypes "github.com/cosmos/gaia/v23/x/liquid/types"
 )
 
+// underBuffer is the number of tokens under the limit to tokenize
+var underBuffer = math.NewInt(5_000_000)
+
 func (s *IntegrationTestSuite) testLiquid() {
 	chainEndpoint := fmt.Sprintf("http://%s", s.valResources[s.chainA.id][0].GetHostPort("1317/tcp"))
 
@@ -22,7 +25,7 @@ func (s *IntegrationTestSuite) testLiquid() {
 
 	validatorAddressA := sdk.ValAddress(validatorAAddr).String()
 
-	s.writeLiquidStakingParamsUpdateProposal(s.chainA)
+	s.writeLiquidStakingParamsUpdateProposal(s.chainA, 25, 50)
 	proposalCounter++
 	submitGovFlags := []string{configFile(proposalLiquidParamUpdateFilename)}
 	depositGovFlags := []string{strconv.Itoa(proposalCounter), depositAmount.String()}
@@ -181,20 +184,255 @@ func (s *IntegrationTestSuite) testLiquid() {
 				return false
 			}
 
-			/*
-				delegationRes, err := queryDelegation(chainEndpoint, validatorAddressA, validatorAAddr.String())
-				delegation := delegationRes.GetDelegationResponse().GetDelegation()
-				s.Require().NoError(err)
-
-				if !delegation.Shares.Equal(selfBondedShares.Add(math.LegacyNewDecFromInt(redeemAmount.Amount))) {
-					return false
-				}
-			*/
-
 			// check that tokenize share record module account received some rewards, since it unbonded during redeem tx execution
 			balanceRes, err = getSpecificBalance(chainEndpoint, tokenizeShareRecord.GetModuleAddress().String(), uatomDenom)
 			s.Require().NoError(err)
 			if balanceRes.Amount.IsNil() || balanceRes.Amount.IsZero() {
+				return false
+			}
+			return true
+		},
+		20*time.Second,
+		5*time.Second,
+	)
+}
+
+// testLiquidGlobalLimit validates the global liquid staking cap
+func (s *IntegrationTestSuite) testLiquidGlobalLimit() {
+	chainEndpoint := fmt.Sprintf("http://%s", s.valResources[s.chainA.id][0].GetHostPort("1317/tcp"))
+
+	validatorA := s.chainA.validators[0]
+	validatorAAddr, _ := validatorA.keyInfo.GetAddress()
+	validatorB := s.chainA.validators[1]
+	validatorBAddr, _ := validatorB.keyInfo.GetAddress()
+
+	validatorAddressB := sdk.ValAddress(validatorBAddr).String()
+
+	s.writeLiquidStakingParamsUpdateProposal(s.chainA, 25, 100)
+	proposalCounter++
+	submitGovFlags := []string{configFile(proposalLiquidParamUpdateFilename)}
+	depositGovFlags := []string{strconv.Itoa(proposalCounter), depositAmount.String()}
+	voteGovFlags := []string{strconv.Itoa(proposalCounter), "yes"}
+
+	// gov proposing Liquid parameters (global liquid staking cap, validator liquid staking cap, validator bond factor)
+	s.T().Logf("Proposal number: %d", proposalCounter)
+	s.T().Logf("Submitting, deposit and vote legacy Gov Proposal: Set parameters (global liquid staking cap, validator liquid staking cap)")
+	s.submitGovProposal(chainEndpoint, validatorAAddr.String(), proposalCounter, "liquidtypes.MsgUpdateProposal",
+		submitGovFlags, depositGovFlags, voteGovFlags, "vote")
+
+	// query the proposal status and new fee
+	s.Require().Eventually(
+		func() bool {
+			proposal, err := queryGovProposal(chainEndpoint, proposalCounter)
+			s.Require().NoError(err)
+			return proposal.GetProposal().Status == govv1beta1.StatusPassed
+		},
+		15*time.Second,
+		5*time.Second,
+	)
+
+	s.Require().Eventually(
+		func() bool {
+			liquidParams, err := queryLiquidParams(chainEndpoint)
+			s.T().Logf("After Liquid parameters update proposal")
+			s.Require().NoError(err)
+
+			s.Require().Equal(liquidParams.Params.GlobalLiquidStakingCap, math.LegacyNewDecWithPrec(25, 2))
+			s.Require().Equal(liquidParams.Params.ValidatorLiquidStakingCap, math.LegacyNewDecWithPrec(100, 2))
+
+			return true
+		},
+		15*time.Second,
+		5*time.Second,
+	)
+	delegatorAddress, _ := s.chainA.genesisAccounts[3].keyInfo.GetAddress()
+
+	fees := sdk.NewCoin(uatomDenom, math.NewInt(1))
+
+	pool, err := queryPool(chainEndpoint)
+	s.Require().NoError(err)
+	tokens, err := queryTotalLiquidStaked(chainEndpoint)
+	s.Require().NoError(err)
+	liquidStakedAmount, err := strconv.ParseInt(tokens, 10, 64)
+	s.Require().NoError(err)
+
+	delegationAmount := math.LegacyNewDec(pool.BondedTokens.Int64()).QuoInt(math.NewInt(4)).Sub(math.LegacyNewDec(
+		liquidStakedAmount)).TruncateInt().Sub(underBuffer)
+	delegation := sdk.NewCoin(uatomDenom, delegationAmount)
+
+	// Alice delegate uatom to Validator A
+	s.execDelegate(s.chainA, 0, delegation.String(), validatorAddressB, delegatorAddress.String(), gaiaHomePath, fees.String())
+
+	// Validate delegation successful
+	s.Require().Eventually(
+		func() bool {
+			res, err := queryDelegation(chainEndpoint, validatorAddressB, delegatorAddress.String())
+			amt := res.GetDelegationResponse().GetDelegation().GetShares()
+			s.Require().NoError(err)
+
+			return amt.Equal(math.LegacyNewDecFromInt(delegationAmount))
+		},
+		20*time.Second,
+		5*time.Second,
+	)
+
+	// Tokenize shares
+	tokenizeAmount := delegationAmount
+	tokenize := sdk.NewCoin(uatomDenom, tokenizeAmount)
+	s.executeTokenizeShares(s.chainA, 0, tokenize.String(), validatorAddressB, delegatorAddress.String(), gaiaHomePath, fees.String())
+
+	recordID, err := queryLastTokenizeShareRecordID(chainEndpoint)
+	s.Require().NoError(err)
+
+	// Validate balance increased
+	shareDenom := fmt.Sprintf("%s/%s", strings.ToLower(validatorAddressB), strconv.Itoa(int(recordID)))
+	s.Require().Eventually(
+		func() bool {
+			res, err := getSpecificBalance(chainEndpoint, delegatorAddress.String(), shareDenom)
+			s.Require().NoError(err)
+			return res.Amount.Equal(tokenizeAmount)
+		},
+		20*time.Second,
+		5*time.Second,
+	)
+
+	// Try to tokenize over the limit
+	secondTokenizeAmount := math.NewInt(5)
+	secondTokenize := sdk.NewCoin(uatomDenom, secondTokenizeAmount)
+	s.executeTokenizeSharesFailure(s.chainA, 0, secondTokenize.String(), validatorAddressB, delegatorAddress.String(),
+		gaiaHomePath, fees.String())
+
+	// Redeem tokens for shares
+	redeemAmount := sdk.NewCoin(shareDenom, tokenizeAmount)
+	s.executeRedeemShares(s.chainA, 0, redeemAmount.String(), delegatorAddress.String(), gaiaHomePath, fees.String())
+
+	// check redeem success
+	s.Require().Eventually(
+		func() bool {
+			balanceRes, err := getSpecificBalance(chainEndpoint, delegatorAddress.String(), shareDenom)
+			s.Require().NoError(err)
+			if !balanceRes.Amount.IsNil() && balanceRes.Amount.IsZero() {
+				return false
+			}
+			return true
+		},
+		20*time.Second,
+		5*time.Second,
+	)
+}
+
+// testLiquidValidatorLimit validates the validator liquid staking cap
+func (s *IntegrationTestSuite) testLiquidValidatorLimit() {
+	chainEndpoint := fmt.Sprintf("http://%s", s.valResources[s.chainA.id][0].GetHostPort("1317/tcp"))
+
+	validatorA := s.chainA.validators[0]
+	validatorAAddr, _ := validatorA.keyInfo.GetAddress()
+	validatorB := s.chainA.validators[1]
+	validatorBAddr, _ := validatorB.keyInfo.GetAddress()
+
+	validatorAddressB := sdk.ValAddress(validatorBAddr).String()
+
+	s.writeLiquidStakingParamsUpdateProposal(s.chainA, 100, 10)
+	proposalCounter++
+	submitGovFlags := []string{configFile(proposalLiquidParamUpdateFilename)}
+	depositGovFlags := []string{strconv.Itoa(proposalCounter), depositAmount.String()}
+	voteGovFlags := []string{strconv.Itoa(proposalCounter), "yes"}
+
+	// gov proposing Liquid parameters (global liquid staking cap, validator liquid staking cap, validator bond factor)
+	s.T().Logf("Proposal number: %d", proposalCounter)
+	s.T().Logf("Submitting, deposit and vote legacy Gov Proposal: Set parameters (global liquid staking cap, validator liquid staking cap)")
+	s.submitGovProposal(chainEndpoint, validatorAAddr.String(), proposalCounter, "liquidtypes.MsgUpdateProposal",
+		submitGovFlags, depositGovFlags, voteGovFlags, "vote")
+
+	// query the proposal status and new fee
+	s.Require().Eventually(
+		func() bool {
+			proposal, err := queryGovProposal(chainEndpoint, proposalCounter)
+			s.Require().NoError(err)
+			return proposal.GetProposal().Status == govv1beta1.StatusPassed
+		},
+		15*time.Second,
+		5*time.Second,
+	)
+
+	s.Require().Eventually(
+		func() bool {
+			liquidParams, err := queryLiquidParams(chainEndpoint)
+			s.T().Logf("After Liquid parameters update proposal")
+			s.Require().NoError(err)
+
+			s.Require().Equal(liquidParams.Params.GlobalLiquidStakingCap, math.LegacyNewDecWithPrec(100, 2))
+			s.Require().Equal(liquidParams.Params.ValidatorLiquidStakingCap, math.LegacyNewDecWithPrec(10, 2))
+
+			return true
+		},
+		15*time.Second,
+		5*time.Second,
+	)
+	delegatorAddress, _ := s.chainA.genesisAccounts[4].keyInfo.GetAddress()
+
+	fees := sdk.NewCoin(uatomDenom, math.NewInt(1))
+
+	valB, err := queryValidator(chainEndpoint, validatorAddressB)
+	s.Require().NoError(err)
+
+	// delegationAmount should put us at exactly 10% of validator's delegator shares
+	// delegationAmount := valB.DelegatorShares.TruncateInt().Quo(math.NewInt(9))
+	delegationAmount := valB.DelegatorShares.Quo(math.LegacyNewDecFromInt(math.NewInt(9))).TruncateInt().Sub(underBuffer)
+	delegation := sdk.NewCoin(uatomDenom, delegationAmount)
+
+	// Alice delegate uatom to Validator B
+	s.execDelegate(s.chainA, 0, delegation.String(), validatorAddressB, delegatorAddress.String(), gaiaHomePath, fees.String())
+
+	// Validate delegation successful
+	s.Require().Eventually(
+		func() bool {
+			res, err := queryDelegation(chainEndpoint, validatorAddressB, delegatorAddress.String())
+			amt := res.GetDelegationResponse().GetDelegation().GetShares()
+			s.Require().NoError(err)
+
+			return amt.Equal(math.LegacyNewDecFromInt(delegationAmount))
+		},
+		20*time.Second,
+		5*time.Second,
+	)
+
+	// Tokenize shares
+	tokenizeAmount := delegationAmount
+	tokenize := sdk.NewCoin(uatomDenom, tokenizeAmount)
+	s.executeTokenizeShares(s.chainA, 0, tokenize.String(), validatorAddressB, delegatorAddress.String(), gaiaHomePath, fees.String())
+
+	recordID, err := queryLastTokenizeShareRecordID(chainEndpoint)
+	s.Require().NoError(err)
+
+	// Validate balance increased
+	shareDenom := fmt.Sprintf("%s/%s", strings.ToLower(validatorAddressB), strconv.Itoa(int(recordID)))
+	s.Require().Eventually(
+		func() bool {
+			res, err := getSpecificBalance(chainEndpoint, delegatorAddress.String(), shareDenom)
+			s.Require().NoError(err)
+			return res.Amount.Equal(tokenizeAmount)
+		},
+		20*time.Second,
+		5*time.Second,
+	)
+
+	// Try to tokenize over the limit
+	secondTokenizeAmount := math.NewInt(5)
+	secondTokenize := sdk.NewCoin(uatomDenom, secondTokenizeAmount)
+	s.executeTokenizeSharesFailure(s.chainA, 0, secondTokenize.String(), validatorAddressB, delegatorAddress.String(),
+		gaiaHomePath, fees.String())
+
+	// Redeem tokens for shares
+	redeemAmount := sdk.NewCoin(shareDenom, tokenizeAmount)
+	s.executeRedeemShares(s.chainA, 0, redeemAmount.String(), delegatorAddress.String(), gaiaHomePath, fees.String())
+
+	// check redeem success
+	s.Require().Eventually(
+		func() bool {
+			balanceRes, err := getSpecificBalance(chainEndpoint, delegatorAddress.String(), shareDenom)
+			s.Require().NoError(err)
+			if !balanceRes.Amount.IsNil() && balanceRes.Amount.IsZero() {
 				return false
 			}
 			return true

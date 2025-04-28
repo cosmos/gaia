f24974d96d0a765a78d50b44c2301a501cace4f1
Eric Warehime <eric.warehime@gmail.com>
2025-04-28 16:50:10 -0700

    chore: Add 23.2.0 upgrade (#3716)
    * Add 23.2.0 upgrade
    
    * Update previous version in upgrade test
    
    * add ibc wasm migrate functionality
    
    * Add test, fix b64 decoding
    
    * Fix lints
    
    ---------
    
    Co-authored-by: Gjermund Garaba <gjermund@garaba.net>

---
+++ app/upgrades/v23_2_0/upgrades.go
@@ -0,0 +1,64 @@
+package v23_2_0 //nolint:revive
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/hex"
+
+	ibcwasmkeeper "github.com/cosmos/ibc-go/modules/light-clients/08-wasm/v10/keeper"
+	ibcwasmtypes "github.com/cosmos/ibc-go/modules/light-clients/08-wasm/v10/types"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	"github.com/cosmos/gaia/v23/app/keepers"
+)
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	keepers *keepers.AppKeepers,
+) upgradetypes.UpgradeHandler {
+	return func(c context.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		ctx := sdk.UnwrapSDKContext(c)
+		ctx.Logger().Info("Starting module migrations...")
+
+		vm, err := mm.RunMigrations(ctx, configurator, vm)
+		if err != nil {
+			return vm, err
+		}
+
+		ctx.Logger().Info("Starting Migrate IBC Wasm...")
+		if err = MigrateIBCWasm(ctx, keepers.WasmClientKeeper, HexChecksum, MigrateMsgBase64, ClientID,
+			SignerAccount); err != nil {
+			ctx.Logger().Info("Error running migrate for IBC Wasm client", "message", err.Error())
+		}
+
+		ctx.Logger().Info("Upgrade v23.2.0 complete")
+		return vm, nil
+	}
+}
+
+func MigrateIBCWasm(ctx sdk.Context, wasmClientKeeper ibcwasmkeeper.Keeper, hexChecksum string,
+	migrateMsgB64 string, clientID string, signerAcc string,
+) error {
+	checksumBz, err := hex.DecodeString(hexChecksum)
+	if err != nil {
+		return err
+	}
+
+	migrateMsgBz, err := base64.RawStdEncoding.DecodeString(migrateMsgB64)
+	if err != nil {
+		return err
+	}
+
+	_, err = wasmClientKeeper.MigrateContract(ctx, &ibcwasmtypes.MsgMigrateContract{
+		Signer:   signerAcc,
+		ClientId: clientID,
+		Checksum: checksumBz,
+		Msg:      migrateMsgBz,
+	})
+	return err
+}

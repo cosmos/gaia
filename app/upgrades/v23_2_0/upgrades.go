986e8410ed7c07d409b848c6b1cf7d53903f26cb
Gjermund Garaba <gjermund@garaba.net>
2025-04-28 22:06:36 +0200

    add ibc wasm migrate functionality

--- app/upgrades/v23_2_0/upgrades.go
+++ app/upgrades/v23_2_0/upgrades.go
@@ -2,6 +2,11 @@ package v23_2_0 //nolint:revive
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/hex"
+
+	ibcwasmkeeper "github.com/cosmos/ibc-go/modules/light-clients/08-wasm/v10/keeper"
+	ibcwasmtypes "github.com/cosmos/ibc-go/modules/light-clients/08-wasm/v10/types"
 
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 
@@ -26,7 +31,7 @@ func CreateUpgradeHandler(
 		}
 
 		ctx.Logger().Info("Starting Migrate IBC Wasm...")
-		if err = MigrateIBCWasm(ctx); err != nil {
+		if err = MigrateIBCWasm(ctx, keepers.WasmClientKeeper); err != nil {
 			ctx.Logger().Info("Error running migrate for IBC Wasm client", "message", err.Error())
 		}
 
@@ -35,6 +40,24 @@ func CreateUpgradeHandler(
 	}
 }
 
-func MigrateIBCWasm(_ sdk.Context) error {
-	return nil
+func MigrateIBCWasm(ctx sdk.Context, wasmClientKeeper ibcwasmkeeper.Keeper) error {
+	hexChecksum := "b92e9904aab2292916507f0db04b7ab6d024c2fdb57a9d52e6725f69b2e684c1"
+	checksumBz, err := hex.DecodeString(hexChecksum)
+	if err != nil {
+		return err
+	}
+
+	migrateMsgBase64 := "eyJtaWdyYXRpb24iOnsidXBkYXRlX2ZvcmtfcGFyYW1ldGVycyI6eyJnZW5lc2lzX2ZvcmtfdmVyc2lvbiI6IjB4MDAwMDAwMDAiLCJnZW5lc2lzX3Nsb3QiOjAsImFsdGFpciI6eyJ2ZXJzaW9uIjoiMHgwMTAwMDAwMCIsImVwb2NoIjo3NDI0MH0sImJlbGxhdHJpeCI6eyJ2ZXJzaW9uIjoiMHgwMjAwMDAwMCIsImVwb2NoIjoxNDQ4OTZ9LCJjYXBlbGxhIjp7InZlcnNpb24iOiIweDAzMDAwMDAwIiwiZXBvY2giOjE5NDA0OH0sImRlbmViIjp7InZlcnNpb24iOiIweDA0MDAwMDAwIiwiZXBvY2giOjI2OTU2OH0sImVsZWN0cmEiOnsidmVyc2lvbiI6IjB4MDUwMDAwMDAiLCJlcG9jaCI6MzY0MDMyfX19fQ"
+	migrateMsgBz, err := base64.StdEncoding.DecodeString(migrateMsgBase64)
+	if err != nil {
+		return err
+	}
+
+	_, err = wasmClientKeeper.MigrateContract(ctx, &ibcwasmtypes.MsgMigrateContract{
+		Signer:   "cosmos10d07y265gmmuvt4z0w9aw880jnsr700j6zn9kn",
+		ClientId: "08-wasm-1369",
+		Checksum: checksumBz,
+		Msg:      migrateMsgBz,
+	})
+	return err
 }
